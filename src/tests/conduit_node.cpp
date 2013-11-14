ec34cda71cddece21898adbb27e2844d486d8ac2
Cyrus Harrison <cyrush@llnl.gov>
2013-11-14 15:46:42 -0800

    updated node utest

--- src/tests/conduit_node.cpp
+++ src/tests/conduit_node.cpp
@@ -26,3 +26,21 @@ TEST(conduit_node_simple_test, conduit_node)
     EXPECT_EQ(n["b"].as_uint32(),b_val);
     EXPECT_EQ(n["c"].as_float64(),c_val);
 }
+
+TEST(conduit_node_simple_schema_test, conduit_node)
+{
+    uint32   a_val  = 10;
+    uint32   b_val  = 20;
+    float64  c_val  = 30.0;
+
+    char *data = new char[16];
+    memcpy(&data[0],&a_val,4);
+    memcpy(&data[4],&b_val,4);
+    memcpy(&data[8],&c_val,8);
+
+    Node n(data,"{\"a\":\"uint32\",\"b\":\"uint32\",\"c\":\"float32\"}");
+
+    EXPECT_EQ(n["a"].as_uint32(),a_val);
+    EXPECT_EQ(n["b"].as_uint32(),b_val);
+    EXPECT_EQ(n["c"].as_float64(),c_val);
+}
