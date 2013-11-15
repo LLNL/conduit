a4605b4bffe91a0137cc78328af6615df915cd0a
Cyrus Harrison <cyrush@llnl.gov>
2013-11-15 11:27:28 -0800

    commit ser check with t node ref

--- src/tests/conduit_serialize.cpp
+++ src/tests/conduit_serialize.cpp
@@ -29,11 +29,13 @@ TEST(conduit_node_simple_test, conduit_node)
     EXPECT_EQ(n["here"]["there"].as_uint32(),a_val);
 
     std::string schema = n.schema();
+    std::cout << "SCHEMA:\n" << schema;
     std::vector<uint8> bytes;
     n.serialize(bytes);
 
 
     Node n2(&bytes[0],schema);
+    Node &t = n2["a"];
     EXPECT_EQ(n2["a"].as_uint32(),a_val);
     EXPECT_EQ(n2["b"].as_uint32(),b_val);
     EXPECT_EQ(n2["c"].as_float64(),c_val);
