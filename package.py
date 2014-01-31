66422ecf25c1bb8db4b43664c504c5f028937668
Cyrus Harrison <cyrush@llnl.gov>
2014-01-31 09:21:25 -0800

    updated package helper to create a path that uses git bundle

--- package.py
+++ package.py
@@ -1,24 +1,27 @@
 #!/bin/env python
 
-
 import subprocess,sys,datetime,os
 
-
-def create_package(output_file=None):
-    if output_file is None:
-        t = datetime.datetime.now()
-        repo_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
-        output_file = "%s.%04d.%02d.%02d.tar" % (repo_name,t.year,t.month,t.day)
-    cmd = "git archive --format=tar --prefix=%s/ HEAD > %s; " % (repo_name,output_file)
-    cmd += "cd ../; tar -rf %s/%s %s/.git; cd %s; " % (repo_name,output_file,repo_name,repo_name)
-    cmd += "gzip %s; " % output_file
+def create_package(bundle=False):
+    t = datetime.datetime.now()
+    repo_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
+    suffix = "tar"
+    if bundle:
+        suffix = "git.bundle"
+    output_file = "%s.%04d.%02d.%02d.%s" % (repo_name,t.year,t.month,t.day,suffix)
+    if bundle:
+        cmd = "git bundle create %s master" % output_file
+    else: # cyrus' old method
+        cmd = "git archive --format=tar --prefix=%s/ HEAD > %s; " % (repo_name,output_file)
+        cmd += "cd ../; tar -rf %s/%s %s/.git; cd %s; " % (repo_name,output_file,repo_name,repo_name)
+        cmd += "gzip %s; " % output_file
     print "[exe: %s]" % cmd
     subprocess.call(cmd,shell=True)
-
+    
 if __name__ == "__main__":
-    output_file = None
+    bundle=True
     if len(sys.argv) > 1:
-        output_file = sys.argv[1]
-    create_package(output_file)
+        bundle = sys.argv[1].lower().startswith("t")
+    create_package(bundle)
 
 
