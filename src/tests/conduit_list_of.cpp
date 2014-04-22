ac48e750d4084d528ffc557cdaf026f9b1a18180
Cyrus Harrison <cyrush@llnl.gov>
2014-04-22 15:20:54 -0700

    added unit test for path ref (will currently fail)

--- src/tests/conduit_list_of.cpp
+++ src/tests/conduit_list_of.cpp
@@ -44,3 +44,42 @@ TEST(conduit_list_of_simple, conduit_list_of)
     EXPECT_EQ(n[1]["b"].as_int32(), b2_val);
 }
 
+TEST(conduit_list_of_path_ref, conduit_list_of)
+{
+    uint32  len_val = 2;
+    int32   a1_val  = 10;
+    int32   b1_val  = 20;
+    
+    int32   a2_val  = -10;
+    int32   b2_val  = -20;
+    
+    char *data = new char[20];
+    memcpy(&data[0],&len_val,4);    
+    memcpy(&data[4],&a1_val,4);
+    memcpy(&data[8],&b1_val,4);
+    memcpy(&data[12],&a2_val,4);
+    memcpy(&data[16],&b2_val,4);
+    
+    std::string jschema = "{ \"list_length\": \"uint32\", \"values\":{\"dtype\":{\"a\":\"int32\",\"b\":\"int32\"},\"length\":{\"reference\":\"../list_length\"}}}";
+	Schema sch(jschema);
+    Node n(sch,data);
+    std::cout << n.schema().to_json() << std::endl;
+    std::cout << n.to_json() << std::endl;
+    
+    std::cout <<  n["list_length"].as_uint32() << std::endl;
+    
+    std::cout <<  n["values"][0]["a"].as_int32() << std::endl;
+    std::cout <<  n["values"][1]["a"].as_int32() << std::endl;
+
+    std::cout <<  n["values"][0]["b"].as_int32() << std::endl;
+    std::cout <<  n["values"][1]["b"].as_int32() << std::endl;
+
+    EXPECT_EQ(n["list_length"].as_uint32(), len_val);
+    EXPECT_EQ(n["values"][0]["a"].as_int32(), a1_val);
+    EXPECT_EQ(n["values"][1]["a"].as_int32(), a2_val);
+
+    EXPECT_EQ(n["values"][0]["b"].as_int32(), b1_val);
+    EXPECT_EQ(n["values"][1]["b"].as_int32(), b2_val);
+}
+
+
