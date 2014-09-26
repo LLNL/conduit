e0e67c02b31cb1215e70c0a0442cf5e090afa459
Cyrus Harrison <cyrush@llnl.gov>
2014-09-26 10:20:13 -0700

    added some cmake settings, including disable of in-source build

---
+++ src/CMake/CMakeBasics.cmake
@@ -0,0 +1,22 @@
+#############################################################################
+# Copyright (c) 2014, Lawrence Livermore National Security, LLC
+# Produced at the Lawrence Livermore National Laboratory. 
+# 
+# All rights reserved.
+# 
+# This source code cannot be distributed without further review from 
+# Lawrence Livermore National Laboratory.
+#############################################################################
+
+################################
+# Standard CMake Options
+################################
+
+
+# Fail if someone tries to config an in-source build.
+if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
+   message(FATAL_ERROR "In-source builds are not supported. Please remove CMakeCache.txt from the 'src' dir and configure an out-of-source build in another directory.")
+endif()
+
+# enable creation of compile_commands.json
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
