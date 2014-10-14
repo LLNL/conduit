33aef3284f21498e337cb69a1f0f9877d0d893d1
Cyrus Harrison <cyrush@llnl.gov>
2014-10-13 20:29:20 -0700

    added disabled test case to io_save_load, this case demos a use case that should work, but has an issue we need to resolve

--- src/tests/conduit_io_save_load.cpp
+++ src/tests/conduit_io_save_load.cpp
@@ -22,9 +22,32 @@ using namespace conduit;
 using namespace std;
 
 
+class ExampleData
+{
+public:
+   ExampleData()
+    {}
+    
+    void alloc(index_t c_size)
+    {
+        x_vals.resize(c_size,0.0);        
+        y_vals.resize(c_size,0.0);
+        z_vals.resize(c_size,0.0);
+        n["x"].set_external(x_vals);
+        n["y"].set_external(y_vals);
+        n["z"].set_external(z_vals);
+    }
+
+   Node n;
+   std::vector<float64> x_vals;
+   std::vector<float64> y_vals;
+   std::vector<float64> z_vals;
+};
+
+
 TEST(conduit_io_save_load, conduit_bin_simple_2_file)
 {
-    return;
+
     int32   a1_val  = 10;
     int32   b1_val  = 20;
     
@@ -111,8 +134,45 @@ TEST(conduit_io_save_load, conduit_mmap_simple_2_file)
    
     // standard read
     
-    Node ntest(schema,"test_conduit_mmap.bin");
+    Node ntest(schema,"test_conduit_mmap_x2.conduit_bin");
     EXPECT_EQ(ntest[0]["a"].as_int32(), 100);
     EXPECT_EQ(ntest[0]["b"].as_int32(), 200);
 }
 
+
+// TODO: Resolve the memory access pattern in this case
+// to run, remove "DISABLED_" prefix
+TEST(conduit_io_save_load, DISABLED_conduit_simple_restore)
+{
+    ExampleData d;
+    d.alloc(10);
+    d.n.print();
+
+    for(index_t i=0;i<10;i++)
+    {
+        d.x_vals[i] = 1.2 * i;
+        d.y_vals[i] = 2.3 * i;
+        d.z_vals[i] = 3.4 * i;
+    }
+
+    d.n.print();
+    d.n.save("test_conduit_restore_mmap");
+
+    ExampleData d2;
+    d2.alloc(10);
+    Node nmmap;
+    nmmap.mmap("test_conduit_restore_mmap");
+
+    d2.n.info().print();
+
+    d2.n.update(nmmap);
+    d2.n.print();
+    // this will fail
+    EXPECT_EQ(d2.n["x"].as_float64_array()[1],d2.x_vals[1]);
+    EXPECT_EQ(d.x_vals[1],d2.x_vals[1]);
+
+    // note that the mem setup isn't quite right, 
+    // we must have fell in to a realloc situation
+    d2.n.info().print();
+
+}
