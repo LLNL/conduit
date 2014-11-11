006b1a30b50e9de27e634a01d2a34e648f3bc360
Cyrus Harrison <cyrush@llnl.gov>
2014-11-11 09:10:47 -0800

    add a few missing cmake changes for conduit_silo

---
+++ src/conduit_silo/conduit_silo.h
@@ -0,0 +1,19 @@
+/*****************************************************************************
+* Copyright (c) 2014, Lawrence Livermore National Security, LLC
+* Produced at the Lawrence Livermore National Laboratory. 
+* 
+* All rights reserved.
+* 
+* This source code cannot be distributed without further review from 
+* Lawrence Livermore National Laboratory.
+*****************************************************************************/
+
+///
+/// file: conduit_silo.h
+///
+
+#include <silo.h>
+#include <conduit.h>
+
+void conduit_silo_placeholder();
+
