ea9f03d76dd0ebab2f0d9b916ffe701549f2071e
Cyrus Harrison <cyrush@llnl.gov>
2014-11-13 10:07:15 -0800

    added missing unit test

---
+++ src/tests/conduit_silo_smoke.cpp
@@ -0,0 +1,22 @@
+/*****************************************************************************
+* Copyright (c) 2014, Lawrence Livermore National Security, LLC
+* Produced at the Lawrence Livermore National Laboratory. 
+* 
+* All rights reserved.
+* 
+* This source code cannot be distributed without further review from 
+* Lawrence Livermore National Laboratory.
+*****************************************************************************/
+
+///
+/// file: silo_smoke.cpp
+///
+
+#include "conduit_silo.h"
+#include <iostream>
+#include "gtest/gtest.h"
+
+TEST(conduit_silo_smoke_test_case, conduit_silo_smoke)
+{
+    conduit_silo_placeholder();
+}
