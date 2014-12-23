c17c8320d620cc18d6616f1c597f2a528590131e
Cyrus Harrison <cyrush@llnl.gov>
2014-12-22 16:44:08 -0800

    added missing header file created in reorg

---
+++ src/conduit_io/conduit_io.h
@@ -0,0 +1,30 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2014, Lawrence Livermore National Security, LLC
+// Produced at the Lawrence Livermore National Laboratory. 
+// 
+// All rights reserved.
+// 
+// This source code cannot be distributed without further review from 
+// Lawrence Livermore National Laboratory.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+//-----------------------------------------------------------------------------
+///
+/// file: conduit_io.h
+///
+//-----------------------------------------------------------------------------
+
+
+#ifndef __CONDUIT_IO_H
+#define __CONDUIT_IO_H
+
+//-----------------------------------------------------------------------------
+// -- define proper lib exports for various platforms -- 
+//-----------------------------------------------------------------------------
+#include "Conduit_IO_Exports.h"
+
+#include "conduit.h"
+#include "conduit_silo.h"
+
+#endif
+
