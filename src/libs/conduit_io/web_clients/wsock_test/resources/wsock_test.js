aa27122b3e8ffe86de181b8220b035f7bca3c67f
Cyrus Harrison <cyrush@llnl.gov>
2015-07-29 16:57:33 -0700

    added missing wsock_test web client

---
+++ src/libs/conduit_io/web_clients/wsock_test/resources/wsock_test.js
@@ -0,0 +1,88 @@
+/*
+###############################################################################
+# Copyright (c) 2014-2015, Lawrence Livermore National Security, LLC.
+# 
+# Produced at the Lawrence Livermore National Laboratory
+# 
+# LLNL-CODE-666778
+# 
+# All rights reserved.
+# 
+# This file is part of Conduit. 
+# 
+# For details, see https://lc.llnl.gov/conduit/.
+# 
+# Please also read conduit/LICENSE
+# 
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice, 
+#   this list of conditions and the disclaimer below.
+# 
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the disclaimer (as noted below) in the
+#   documentation and/or other materials provided with the distribution.
+# 
+# * Neither the name of the LLNS/LLNL nor the names of its contributors may
+#   be used to endorse or promote products derived from this software without
+#   specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL SECURITY,
+# LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+# DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+###############################################################################
+*/
+
+function websocket_test()
+{
+    var wsproto = (location.protocol === 'https:') ? 'wss:' : 'ws:';
+    connection = new WebSocket(wsproto + '//' + window.location.host + '/websocket');
+    
+    connection.onmessage = function (msg) 
+    {
+        var data;
+        try
+        {
+            data=JSON.parse(msg.data);
+        }
+        catch(e)
+        {
+            alert(e); //error in the above string(in this case,yes)!
+        }
+
+        if(data.type == "image")
+        {
+            $("#image_display").html("<img src='" + data.data + "'/>");
+            $("#image_display").show();
+            $("#count_display").text("Current Count = " +data.count);
+                        
+            var response = {type: "info",
+                            message: "response from browser, count = " + data.count};
+
+            connection.send(JSON.stringify(response));
+        }
+    }
+      
+    connection.onerror = function (error)
+    {
+        console.log('WebSocket error');
+        connection.close();
+    }
+}
+
+websocket_test();
+
+
+
+
