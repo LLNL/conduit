2dd2284ff097f92b6e04705a2ddba1b5d92e4e8d
Cyrus Harrison <cyrush@llnl.gov>
2016-03-14 21:27:40 -0700

    host-config: update llnl cz chaos5 tpls

--- host-configs/chaos_5_x86_64_ib-gcc@4.9.3.cmake
+++ host-configs/chaos_5_x86_64_ib-gcc@4.9.3.cmake
@@ -5,7 +5,7 @@
 ##################################
 
 # cmake from uberenv
-# cmake exectuable path: /usr/gapps/conduit/thirdparty_libs/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/cmake-3.4.0-bupmqgyltb5wenlmm57ze3h43uqg5lx5/bin/cmake
+# cmake exectuable path: /usr/gapps/conduit/thirdparty_libs/stable/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/cmake-3.3.1-3gc4unffj5rqcq35gecg4wv3roecpldt/bin/cmake
 
 #######
 # using gcc@4.9.3 compiler spec
@@ -26,16 +26,16 @@ set(CMAKE_Fortran_COMPILER  "/usr/apps/gnu/4.9.3/bin/gfortran" CACHE PATH "")
 set(ENABLE_PYTHON ON CACHE PATH "")
 
 # python from uberenv
-set(PYTHON_EXECUTABLE "/usr/gapps/conduit/thirdparty_libs/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/python-2.7.8-wxs6x4molggy6niut2czpjfhvhmrscj5/bin/python" CACHE PATH "")
+set(PYTHON_EXECUTABLE "/usr/gapps/conduit/thirdparty_libs/stable/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/python-2.7.11-eujx7frnxd5vpwolmye2fzq4tcylnbnv/bin/python" CACHE PATH "")
 
 # sphinx from uberenv
-set(SPHINX_EXECUTABLE "/usr/gapps/conduit/thirdparty_libs/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/python-2.7.8-wxs6x4molggy6niut2czpjfhvhmrscj5/bin/sphinx-build" CACHE PATH "")
+set(SPHINX_EXECUTABLE "/usr/gapps/conduit/thirdparty_libs/stable/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/python-2.7.11-eujx7frnxd5vpwolmye2fzq4tcylnbnv/bin/sphinx-build" CACHE PATH "")
 
 # python3 from uberenv
-#set(PYTHON_EXECUTABLE "/usr/gapps/conduit/thirdparty_libs/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/python3-3.4.3-cy7dff7xqjqon57u56h5atc6kkfxcilc/bin/python3" CACHE PATH "")
+#set(PYTHON_EXECUTABLE "/usr/gapps/conduit/thirdparty_libs/stable/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/python3-3.5.1-bbbluvapi4elzb76vqqriuwq3y7ldqkk/bin/python3" CACHE PATH "")
 
 # sphinx from uberenv
-#set(SPHINX_EXECUTABLE "/usr/gapps/conduit/thirdparty_libs/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/python3-3.4.3-cy7dff7xqjqon57u56h5atc6kkfxcilc/bin/sphinx-build" CACHE PATH "")
+#set(SPHINX_EXECUTABLE "/usr/gapps/conduit/thirdparty_libs/stable/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/python3-3.5.1-bbbluvapi4elzb76vqqriuwq3y7ldqkk/bin/sphinx-build" CACHE PATH "")
 
 # MPI Support
 set(ENABLE_MPI ON CACHE PATH "")
@@ -46,26 +46,18 @@ set(MPI_Fortran_COMPILER "/usr/local/bin/mpif90" CACHE PATH "")
 
 # I/O Packages
 
-# Enable HDF5 Support in conduit_io
-set(ENABLE_HDF5 ON CACHE PATH "")
-
 # hdf5 from uberenv
-set(HDF5_DIR "/usr/gapps/conduit/thirdparty_libs/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/hdf5-1.8.16-rmyziv2il5qhgnpntj2dsr7qusrn7ual" CACHE PATH "")
-
-# Enable Silo Support in conduit_io
-set(ENABLE_SILO ON CACHE PATH "")
+set(HDF5_DIR "/usr/gapps/conduit/thirdparty_libs/stable/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/hdf5-1.8.16-msbowehgkgvhlnl62fy6tb7bvefbr7h4" CACHE PATH "")
 
 # silo from uberenv
-set(SILO_DIR "/usr/gapps/conduit/thirdparty_libs/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/silo-4.10.1-seumccglv6ja5dv2myyxatsjhterselh" CACHE PATH "")
+set(SILO_DIR "/usr/gapps/conduit/thirdparty_libs/stable/spack/opt/spack/chaos_5_x86_64_ib/gcc-4.9.3/silo-4.10.1-jnuhe4xm3vtwq4mevsobhahlriuqafrg" CACHE PATH "")
 
 ##################################
 # end uberenv host-config
 ##################################
 
-
-# Enable mpi for conduit-mpi
-set(ENABLE_MPI ON CACHE PATH "")
+# Extra MPI Settings for Chaos
 set(MPIEXEC /usr/bin/srun CACHE PATH "")
-set(MPIEXEC_NUMPROC_FLAG -p pdebug -n CACHE PATH "")
+set(MPIEXEC_NUMPROC_FLAG -n CACHE PATH "")
 
 
