4fd4c02f01a087954f1a578d01dce5c3c3260746
Cyrus Harrison <cyrush@llnl.gov>
2016-04-01 16:29:06 -0700

    bugfix: case issues with includes in headers

--- src/libs/blueprint/blueprint_mcarray.hpp
+++ src/libs/blueprint/blueprint_mcarray.hpp
@@ -55,7 +55,7 @@
 // conduit lib includes
 //-----------------------------------------------------------------------------
 #include "conduit.hpp"
-#include "Blueprint_Exports.hpp"
+#include "blueprint_exports.hpp"
 
 
 //-----------------------------------------------------------------------------
