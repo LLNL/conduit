6f41c63a2a96534480304c9a28c30caaedf18f88
Joseph Ciurej <ciurej1@llnl.gov>
2016-06-07 16:41:39 -0700

    Implemented test cases for the 'conduit::Node::paths()' function.

--- src/tests/conduit/t_conduit_node_paths.cpp
+++ src/tests/conduit/t_conduit_node_paths.cpp
@@ -50,6 +50,9 @@
 
 #include "conduit.hpp"
 
+#include <vector>
+#include <set>
+#include <string>
 #include <iostream>
 #include "gtest/gtest.h"
 
@@ -69,28 +72,61 @@ TEST(conduit_node_paths, simple_path)
 
     Schema schema("{\"a\":\"uint32\",\"b\":\"uint32\",\"c\":\"float64\"}");
     Node n(schema,data,true);
-    std::cout << n.schema().to_json() <<std::endl; 
-    
+    std::cout << n.schema().to_json() << std::endl; 
+
     EXPECT_TRUE(n.has_path("a"));
     EXPECT_EQ(n.fetch("a").as_uint32(),a_val);
-    
+
     EXPECT_TRUE(n.has_path("b"));
     EXPECT_EQ(n.fetch("b").as_uint32(),b_val);
-    
+
     EXPECT_TRUE(n.has_path("c"));
     EXPECT_EQ(n.fetch("c").as_float64(),c_val);
 
     Schema schema2("{\"g\": {\"a\":\"uint32\",\"b\":\"uint32\",\"c\":\"float64\"}}");
     Node n2(schema2,data,true);
-    std::cout << n2.schema().to_json() <<std::endl; 
+    std::cout << n2.schema().to_json() << std::endl; 
     EXPECT_TRUE(n2.has_path("g/a"));
     EXPECT_EQ(n2.fetch("g/a").as_uint32(),a_val);
     EXPECT_TRUE(n2.has_path("g/b"));
     EXPECT_EQ(n2.fetch("g/b").as_uint32(),b_val);
     EXPECT_TRUE(n2.has_path("g/c"));
     EXPECT_EQ(n2.fetch("g/c").as_float64(),c_val);
-
     EXPECT_FALSE(n.has_path("g/d"));
+
+    delete [] data;
+}
+
+//-----------------------------------------------------------------------------
+TEST(conduit_node_paths, simple_paths)
+{
+    uint32   a_val  = 10;
+    uint32   b_val  = 20;
+    float64  c_val  = 30.0;
+
+    char *data = new char[16];
+    memcpy(&data[0],&a_val,4);
+    memcpy(&data[4],&b_val,4);
+    memcpy(&data[8],&c_val,8);
+
+    Schema schema("{\"a\":\"uint32\",\"b\":\"uint32\",\"c\":\"float64\"}");
+    Node n(schema,data,true);
+    std::cout << n.schema().to_json() << std::endl; 
+    const std::vector<std::string>& npaths = n.paths();
+    EXPECT_EQ(npaths.size(),3);
+    const std::set<std::string> npaths_set(npaths.begin(),npaths.end());
+    std::set<std::string> npaths_set_expected;
+    npaths_set_expected.insert("a");
+    npaths_set_expected.insert("b");
+    npaths_set_expected.insert("c");
+    EXPECT_EQ(npaths_set,npaths_set_expected);
+
+    Schema schema2("{\"g\": {\"a\":\"uint32\",\"b\":\"uint32\",\"c\":\"float64\"}}");
+    Node n2(schema2,data,true);
+    std::cout << n2.schema().to_json() << std::endl;
+    const std::vector<std::string>& n2paths = n2.paths();
+    EXPECT_EQ(n2paths.size(),1);
+    EXPECT_EQ(n2paths[0],"g");
 
     delete [] data;
 }
