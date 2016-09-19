bc6f03b123508baff7c562e65a2709922a0d28d4
Cyrus Harrison <cyrush@llnl.gov>
2016-09-19 11:47:49 -0700

    bugfix: fix typo due to arg name change

--- src/libs/blueprint/blueprint_mcarray_examples.cpp
+++ src/libs/blueprint/blueprint_mcarray_examples.cpp
@@ -233,7 +233,7 @@ xyz(const std::string &mcarray_type,
     }
     else
     {
-        CONDUIT_ERROR("unknown mca_type = " << mca_type);
+        CONDUIT_ERROR("unknown mca_type = " << mcarray_type);
     }
 }
 
