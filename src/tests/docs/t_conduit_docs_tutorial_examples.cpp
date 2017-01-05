f6925db5e3828b7d702ca14d901c668aac6550d6
Cyrus Harrison <cyrush@llnl.gov>
2017-01-05 11:59:30 -0800

    enh: added tutorial section on error handling (#130)

--- src/tests/docs/t_conduit_docs_tutorial_examples.cpp
+++ src/tests/docs/t_conduit_docs_tutorial_examples.cpp
@@ -466,3 +466,78 @@ TEST(conduit_tutorial, mem_ownership_external)
 }
 
 
+
+//-----------------------------------------------------------------------------
+// 472-496
+
+void my_info_handler(const std::string &msg,
+                     const std::string &file,
+                     int line)
+{
+    std::cout << "[INFO] " << msg << std::endl;
+}
+
+void my_warning_handler(const std::string &msg,
+                        const std::string &file,
+                        int line)
+{
+    std::cout << "[WARNING!] " << msg << std::endl;
+}
+
+void my_error_handler(const std::string &msg,
+                      const std::string &file,
+                      int line)
+{
+    std::cout << "[ERROR!] " << msg << std::endl;
+    // errors are considered fatal, aborting or unwinding the 
+    // call stack with an exception are the only viable options
+    throw conduit::Error(msg,file,line);
+}
+
+//-----------------------------------------------------------------------------
+// 502-530 +  531-535
+
+TEST(conduit_tutorial, error_handlers)
+{
+    // rewire error handlers
+    conduit::utils::set_info_handler(my_info_handler);
+    conduit::utils::set_warning_handler(my_warning_handler);
+    conduit::utils::set_error_handler(my_error_handler);
+    
+    // emit an example info message
+    CONDUIT_INFO("An info message");
+    
+    Node n;
+    n["my_value"].set_float64(42.0);
+    
+    // emit an example warning message
+    
+    // using "as" for wrong type emits a warning, returns a default value (0.0)
+    float32 v = n["my_value"].as_float32();
+    
+    // emit an example error message
+
+    try
+    {
+        // fetching a non-existant path from a const Node emits an error
+        const Node &n_my_value = n["my_value"];
+        n_my_value["bad"];
+    }
+    catch(conduit::Error e)
+    {
+        // pass
+    }
+    
+    // restore default handlers
+    conduit::utils::set_info_handler(conduit::utils::default_info_handler);
+    conduit::utils::set_warning_handler(conduit::utils::default_warning_handler);
+    conduit::utils::set_error_handler(conduit::utils::default_error_handler);
+
+}
+
+
+
+
+
+
+
