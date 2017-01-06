fcc5b937356a190494f44d4a1ccf6d2193f9f09d
Cyrus Harrison <cyrush@llnl.gov>
2017-01-06 10:48:09 -0800

    update version and copyright date (#131)

--- src/docs/sphinx/conf.py
+++ src/docs/sphinx/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 ###############################################################################
-# Copyright (c) 2014-2016, Lawrence Livermore National Security, LLC.
+# Copyright (c) 2014-2017, Lawrence Livermore National Security, LLC.
 #
 # Produced at the Lawrence Livermore National Laboratory
 #
@@ -101,7 +101,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Conduit'
-copyright = u'2014-2016, LLNS'
+copyright = u'2014-2017, LLNS'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
