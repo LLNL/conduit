185d28fd9404dd090fb152ed9ee071d382d1fffa
Kathleen Biagas <biagas2@llnl.gov>
2017-02-07 12:56:10 -0800

    Use find_package with CONFIG signature to find HDF5 on windows.

--- src/CMake/thirdparty/SetupHDF5.cmake
+++ src/CMake/thirdparty/SetupHDF5.cmake
@@ -53,13 +53,25 @@ endif()
 
 # CMake's FindHDF5 module uses the HDF5_ROOT env var
 set(HDF5_ROOT ${HDF5_DIR})
-set(ENV{HDF5_ROOT} ${HDF5_ROOT}/bin)
 
-# Use CMake's FindHDF5 module, which uses hdf5's compiler wrappers to extract
-# all the info about the hdf5 install
-include(FindHDF5)
+if(NOT WIN32)
+    set(ENV{HDF5_ROOT} ${HDF5_ROOT}/bin)
+    # Use CMake's FindHDF5 module, which uses hdf5's compiler wrappers to extract
+    # all the info about the hdf5 install
+    include(FindHDF5)
 
-# FindHDF5 sets HDF5_DIR to it's installed CMake info if it exists
+else()
+    # CMake's FindHDF5 module is buggy on windows and will put the dll
+    # in HDF5_LIBRARY.  Instead, use the 'CONFIG' signature of find_package
+    # with appropriate hints for where cmake can find hdf5-config.cmake.
+    find_package(HDF5 CONFIG 
+                 HINTS ${HDF5_DIR}/cmake/hdf5 
+                       ${HDF5_DIR}/lib/cmake/hdf5
+                       ${HDF5_DIR}/share/cmake/hdf5)
+endif()
+
+
+# FindHDF5/find_package sets HDF5_DIR to it's installed CMake info if it exists
 # we want to keep HDF5_DIR as the root dir of the install to be 
 # consistent with other packages
 
