e625f0d689f681ea344f9b8025961c2531793482
Cyrus Harrison <cyrush@llnl.gov>
2017-02-07 23:07:09 -0800

    try to use prefix and import_prefix target props to avoid extra cfg dirs in python modules on windows

--- src/CMake/thirdparty/SetupPython.cmake
+++ src/CMake/thirdparty/SetupPython.cmake
@@ -159,6 +159,8 @@ FUNCTION(PYTHON_ADD_COMPILED_MODULE target_name
                                     py_module_dir)
     MESSAGE(STATUS "Configuring python module: ${target_name}")
     PYTHON_ADD_MODULE(${target_name} ${ARGN})
+    SET_TARGET_PROPERTIES(${target_name} PROPERTIES PREFIX "../")
+    SET_TARGET_PROPERTIES(${target_name} PROPERTIES IMPORT_PREFIX "../")
     SET_TARGET_PROPERTIES(${target_name} PROPERTIES
                                          LIBRARY_OUTPUT_DIRECTORY
                                          ${CMAKE_BINARY_DIR}/${dest_dir}/${py_module_dir})
@@ -197,6 +199,8 @@ FUNCTION(PYTHON_ADD_HYBRID_MODULE target_name
                                ${setup_file}
                                ${py_sources})
     PYTHON_ADD_MODULE(${target_name} ${ARGN})
+    SET_TARGET_PROPERTIES(${target_name} PROPERTIES PREFIX "../")
+    SET_TARGET_PROPERTIES(${target_name} PROPERTIES IMPORT_PREFIX "../")
     SET_TARGET_PROPERTIES(${target_name} PROPERTIES
                                          LIBRARY_OUTPUT_DIRECTORY
                                          ${CMAKE_BINARY_DIR}/${dest_dir}/${py_module_dir})
