c458e71c14c87f2fbe52c2099c691a15685dc310
Cyrus Harrison <cyrush@llnl.gov>
2017-07-22 15:20:31 -0700

    update package helper script (#199)

--- package.py
+++ package.py
@@ -55,13 +55,17 @@ import sys
 import datetime
 import os
 
+from os.path import join as pjoin
+
 def create_package(output_file=None):
+    scripts_dir = pjoin(os.path.abspath(os.path.split(__file__)[0]),"scripts")
+    pkg_script = pjoin(scripts_dir,"git_archive_all.py");
     repo_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
     if output_file is None:
-        suffix = "tar";
-        t = datetime.datetime.now()
-        output_file = "%s.%04d.%02d.%02d.%s" % (repo_name,t.year,t.month,t.day,suffix)
-    cmd = "git archive --format=tar --prefix=%s/ HEAD > %s " % (repo_name,output_file)
+         suffix = "tar"
+         t = datetime.datetime.now()
+         output_file = "%s.%04d.%02d.%02d.%s" % (repo_name,t.year,t.month,t.day,suffix)
+    cmd = "python " + pkg_script + " --prefix=conduit " + output_file
     print "[exe: %s]" % cmd
     subprocess.call(cmd,shell=True)
     
