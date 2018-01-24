163d6745ef04c85cb37f64227ae7a4b744742d3a
Cyrus Harrison <cyrush@llnl.gov>
2018-01-23 17:49:05 -0800

    test installed using examples instead those in src

--- scripts/ci/travis-test-build-examples-vs-install.sh
+++ scripts/ci/travis-test-build-examples-vs-install.sh
@@ -56,7 +56,7 @@ fi
 ##########################################
 # using with cmake example
 ##########################################
-cd ${TRAVIS_BUILD_DIR}/src/examples/using-with-cmake
+cd ${TRAVIS_BUILD_DIR}/travis-debug-install/examples/using-with-cmake
 mkdir build
 cd build
 cmake -DCONDUIT_DIR=${TRAVIS_BUILD_DIR}/travis-debug-install ../
@@ -67,7 +67,7 @@ make
 ##########################################
 # find spack installed HDF5_DIR
 export HDF5_DIR=`ls -d ${TRAVIS_BUILD_DIR}/uberenv_libs/spack/opt/spack/*/*/hdf5*`
-cd ${TRAVIS_BUILD_DIR}/src/examples/using-with-make
+cd ${TRAVIS_BUILD_DIR}/travis-debug-install/examples/using-with-make
 env CXX=${COMPILER_CXX} CONDUIT_DIR=${TRAVIS_BUILD_DIR}/travis-debug-install HDF5_DIR=${HDF5_DIR} make
 env LD_LIBRARY_PATH=${TRAVIS_BUILD_DIR}/travis-debug-install/lib/:${HDF5_DIR}/lib ./example
 
