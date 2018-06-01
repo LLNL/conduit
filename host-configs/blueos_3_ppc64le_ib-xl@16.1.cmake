7c9fda77cd711aadfa8660e06c582cf7de0dc0d0
Chris White <white238@llnl.gov>
2018-06-01 16:53:01 -0700

    Fix XL build issue in tests, Add basic BlueOS XL host-config, remove old compiler file

---
+++ host-configs/blueos_3_ppc64le_ib-xl@16.1.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_C_COMPILER "/usr/tce/packages/xl/xl-2018.05.18/xlC/16.1.0/bin/xlc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/xl/xl-2018.05.18/xlC/16.1.0/bin/xlC" CACHE PATH "")
+set(CMAKE_Fortran_COMPILER "/usr/tce/packages/xl/xl-2018.05.18/xlf/16.1.0/bin/xlf" CACHE PATH "")
+
+set(ENABLE_FORTRAN ON CACHE BOOL "")
