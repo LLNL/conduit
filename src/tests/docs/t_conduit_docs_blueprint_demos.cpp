4e09454f79f6b833c3e28ada12a773a57cfe6634
Cyrus Harrison <cyrush@llnl.gov>
2018-08-10 12:03:06 -0500

    provide simple unif mesh bp example and docs refactor (#326)

--- src/tests/docs/t_conduit_docs_blueprint_demos.cpp
+++ src/tests/docs/t_conduit_docs_blueprint_demos.cpp
@@ -507,3 +507,70 @@ TEST(conduit_docs, blueprint_demo_basic_hexs)
 
     validate_basic_example("hexs",mesh,mesh_json);
 }
+
+
+//-----------------------------------------------------------------------------
+TEST(conduit_docs, blueprint_demo_basic_uniform_detailed)
+{
+    // create a Conduit node to hold our mesh data
+    Node mesh;
+    
+    // create the coordinate set
+    mesh["coordsets/coords/type"] = "uniform";
+    mesh["coordsets/coords/dims/i"] = 3;
+    mesh["coordsets/coords/dims/j"] = 3;
+    // add origin and spacing to the coordset (optional)
+    mesh["coordsets/coords/origin/x"] = -10.0;
+    mesh["coordsets/coords/origin/y"] = -10.0;
+    mesh["coordsets/coords/spacing/dx"] = 10.0;
+    mesh["coordsets/coords/spacing/dy"] = 10.0;
+    
+    // add the topology
+    // this case is simple b/c it's implicitly derived from the coordinate set
+    mesh["topologies/topo/type"] = "uniform";
+    // reference the coordinate set by name
+    mesh["topologies/topo/coordset"] = "coords";
+    
+    // add a simple element-associated field 
+    mesh["fields/ele_example/association"] =  "element";
+    // reference the topology this field is defined on by name
+    mesh["fields/ele_example/topology"] =  "topo";
+    // set the field values, for this case we have 4 elements
+    mesh["fields/ele_example/values"].set(DataType::float64(4));
+    
+    float64 *ele_vals_ptr = mesh["fields/ele_example/values"].value();
+    
+    for(int i=0;i<4;i++)
+    {
+        ele_vals_ptr[i] = float64(i);
+    }
+    
+    // add a simple vertex-associated field 
+    mesh["fields/vert_example/association"] =  "vertex";
+    // reference the topology this field is defined on by name
+    mesh["fields/vert_example/topology"] =  "topo";
+    // set the field values, for this case we have 9 vertices
+    mesh["fields/vert_example/values"].set(DataType::float64(9));
+    
+    float64 *vert_vals_ptr = mesh["fields/vert_example/values"].value();
+    
+    for(int i=0;i<9;i++)
+    {
+        vert_vals_ptr[i] = float64(i);
+    }
+    
+    // make sure we conform:
+    Node verify_info;
+    if(!blueprint::mesh::verify(mesh, verify_info))
+    {
+        std::cout << "Verify failed!" << std::endl;
+        verify_info.print();
+    }
+
+    // print out results
+    mesh.print();
+    
+    // save our mesh to a json that can be read by VisIt
+    conduit::relay::io_blueprint::save(mesh, "basic_detailed_uniform.blueprint_root");
+}
+
