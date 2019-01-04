5202f59074df0db2f328acdd73227f98c06300cf
Cyrus Harrison <cyrush@llnl.gov>
2019-01-04 13:39:36 -0800

    update uberenv and spack settings (#356)
    * update uber and spack configs
    
    * add test running option to uber
    
    * add uber+spack test scripts
    
    * add spack env helper script
    
    * allow tests to append to existing PYTHONPATH in the CMake build system
    
    * spack config updates for llnl systems
    
    * update building docs to talk about super quick start

---
+++ scripts/spack_build_tests/llnl_toss3_gcc.sh
@@ -0,0 +1,3 @@
+python uberenv/uberenv.py --install --run_tests --prefix="_uberenv_test_toss3_gcc"
+
+
---
+++ scripts/spack_build_tests/llnl_toss3_gcc_static.sh
@@ -0,0 +1,3 @@
+python uberenv/uberenv.py --install --run_tests --spec="%gcc~shared" --prefix="_uberenv_test_toss3_gcc_static"
+
+
