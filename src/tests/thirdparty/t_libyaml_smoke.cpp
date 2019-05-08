5116984d2844671299000521613f5ff12f5f7c9d
Cyrus Harrison <cyrush@llnl.gov>
2019-05-08 14:25:06 -0700

    libyaml smoke test: add doc parsing api test

--- src/tests/thirdparty/t_libyaml_smoke.cpp
+++ src/tests/thirdparty/t_libyaml_smoke.cpp
@@ -52,7 +52,7 @@
 #include "gtest/gtest.h"
 
 //-----------------------------------------------------------------------------
-TEST(libyaml_smoke, basic_use)
+TEST(libyaml_smoke, basic_event_use)
 {
     yaml_parser_t parser;
     yaml_event_t  event;
@@ -90,6 +90,7 @@ TEST(libyaml_smoke, basic_use)
             {
                 found_scalar = true;
                 std::string val((const char*)event.data.scalar.value);
+                std::cout << val << std::endl;
                 if(val == "hello")
                 {
                     found_hello = true;
@@ -117,3 +118,53 @@ TEST(libyaml_smoke, basic_use)
     // cleanup parser
     yaml_parser_delete(&parser);
 }
+
+//-----------------------------------------------------------------------------
+TEST(libyaml_smoke, basic_document_use)
+{
+    yaml_parser_t parser;
+    yaml_document_t document;
+
+    const char yaml_str[] = " hello : world\n line: 2\n";
+
+    // Initialize parser
+    EXPECT_TRUE(yaml_parser_initialize(&parser));
+
+    // set input
+    yaml_parser_set_input_string(&parser,
+                                 (const unsigned char*)yaml_str,
+                                 strlen(yaml_str));
+
+    // construct document
+    EXPECT_TRUE(yaml_parser_load(&parser, &document));
+    yaml_node_t *node = yaml_document_get_root_node(&document);
+
+    EXPECT_TRUE(node != NULL);
+
+    // for this example, we we know the root is a mapping node
+    EXPECT_TRUE( node->type == YAML_MAPPING_NODE );
+
+    yaml_node_pair_t *pair = node->data.mapping.pairs.start;
+    EXPECT_TRUE(pair != NULL);
+
+    yaml_node_t *key = yaml_document_get_node(&document, pair->key);
+    yaml_node_t *val = yaml_document_get_node(&document, pair->value);
+
+    EXPECT_TRUE(key != NULL);
+    EXPECT_TRUE(val != NULL);
+    
+    std::string key_name((const char*)key->data.scalar.value);
+    std::string val_str((const char*)val->data.scalar.value);
+
+    std::cout << key_name << ": " << val_str << std::endl;
+
+    EXPECT_EQ(key_name,"hello");
+    EXPECT_EQ(val_str,"world");
+
+    // cleanup doc
+    yaml_document_delete(&document);
+
+    // cleanup parser
+    yaml_parser_delete(&parser);
+}
+
