6fb1becf30e5ea70287ecabe98545e46c6796b2e
Noah S. Elliott <elliott22@llnl.gov>
2019-10-21 14:35:18 -0700

    Add ratio for adjacency neighbors on different AMR levels

--- src/libs/blueprint/conduit_blueprint_mesh.cpp
+++ src/libs/blueprint/conduit_blueprint_mesh.cpp
@@ -4142,15 +4142,22 @@ mesh::adjset::verify(const Node &adjset,
                         wndw_info, "dims") &&
                         mesh::logical_dims::verify(wndw["dims"],
                             wndw_info["dims"]);
+                    window_res &= verify_field_exists(protocol, wndw,
+                        wndw_info, "ratio") &&
+                        mesh::logical_dims::verify(wndw["ratio"],
+                            wndw_info["ratio"]);
 
-                    // one last pass: verify that dimensions for "origin", and
-                    // "dims" are the same
+                    // verify that dimensions for "origin" and
+                    // "dims" and "ratio" are the same
                     if(window_res)
                     {
                         index_t window_dim = wndw["origin"].number_of_children();
                         window_res &= !wndw.has_child("dims") ||
                             verify_object_field(protocol, wndw,
                                 wndw_info, "dims", false, window_dim);
+                        window_res &= !wndw.has_child("ratio") ||
+                            verify_object_field(protocol, wndw,
+                                wndw_info, "ratio", false, window_dim);
                     }
 
                     log::validation(wndw_info,window_res);
