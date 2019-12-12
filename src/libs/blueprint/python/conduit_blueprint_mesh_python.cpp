0fe7ba45add694a8d4a9b55a781e79fceea36b29
Cyrus Harrison <cyrush@llnl.gov>
2019-12-11 21:33:22 -0800

    more appveyor tweaks

--- src/libs/blueprint/python/conduit_blueprint_mesh_python.cpp
+++ src/libs/blueprint/python/conduit_blueprint_mesh_python.cpp
@@ -423,9 +423,9 @@ static struct PyModuleDef blueprint_mesh_python_module_def =
 extern "C" 
 //---------------------------------------------------------------------------//
 #if defined(IS_PY3K)
-PyObject * CONDUIT_BLUEPRINT_PYTHON_API PyInit_conduit_blueprint_mesh_python(void)
+CONDUIT_BLUEPRINT_PYTHON_API PyObject *PyInit_conduit_blueprint_mesh_python(void)
 #else
-void CONDUIT_BLUEPRINT_PYTHON_API initconduit_blueprint_mesh_python(void)
+CONDUIT_BLUEPRINT_PYTHON_API void initconduit_blueprint_mesh_python(void)
 #endif
 //---------------------------------------------------------------------------//
 {    
--- src/libs/relay/python/conduit_relay_web_python.cpp
+++ src/libs/relay/python/conduit_relay_web_python.cpp
@@ -984,9 +984,9 @@ static struct PyModuleDef relay_web_python_module_def =
 extern "C" 
 //---------------------------------------------------------------------------//
 #if defined(IS_PY3K)
-PyObject *CONDUIT_RELAY_PYTHON_API PyInit_conduit_relay_web_python(void)
+CONDUIT_RELAY_PYTHON_API PyObject * PyInit_conduit_relay_web_python(void)
 #else
-void CONDUIT_RELAY_PYTHON_API initconduit_relay_web_python(void)
+CONDUIT_RELAY_PYTHON_API void initconduit_relay_web_python(void)
 #endif
 //---------------------------------------------------------------------------//
 {    
