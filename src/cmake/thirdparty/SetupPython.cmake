c4a29c9f7a755555ad4530e6f2067163316801d4
Cyrus Harrison <cyrush@llnl.gov>
2019-12-30 14:19:22 -0800

    py lib another fix for glob multi value res

--- src/cmake/thirdparty/SetupPython.cmake
+++ src/cmake/thirdparty/SetupPython.cmake
@@ -120,7 +120,14 @@ if(PYTHONINTERP_FOUND)
             FILE(GLOB PYTHON_GLOB_RESULT ${PYTHON_GLOB_TEST})
         endif()
 
-        FILE(GLOB PYTHON_GLOB_RESULT ${PYTHON_GLOB_TEST})            
+        FILE(GLOB PYTHON_GLOB_RESULT ${PYTHON_GLOB_TEST})
+
+        #glob result might be a list, if so select the first entry as py lib
+        list(LENGTH PYTHON_GLOB_RESULT PYTHON_GLOB_RESULT_LEN)
+        if(${PYTHON_GLOB_RESULT_LEN} GREATER 1)
+            list(GET PYTHON_GLOB_RESULT 0 PYTHON_GLOB_RESULT)
+        endif()
+
         get_filename_component(PYTHON_LIBRARY "${PYTHON_GLOB_RESULT}" ABSOLUTE)
 
         if(NOT EXISTS ${PYTHON_LIBRARY})
