43e62371cfedc76b17af594ced68998a6a8feba7
Cyrus Harrison <cyrush@llnl.gov>
2020-01-02 15:26:20 -0800

    fix typo

--- src/cmake/thirdparty/SetupPython.cmake
+++ src/cmake/thirdparty/SetupPython.cmake
@@ -128,8 +128,8 @@ if(PYTHONINTERP_FOUND)
             # the glob result might be a list due to symlinks, etc
             # if it is a list, select the first entry as py lib
             list(LENGTH PYTHON_GLOB_RESULT PYTHON_GLOB_RESULT_LEN)
-            if(${PYTHON_GLOB_RESULT_LEN} GREATER 0)
-                list(GET PYTHON_GLOB_RESULT 1 PYTHON_GLOB_RESULT)
+            if(${PYTHON_GLOB_RESULT_LEN} GREATER 1)
+                list(GET PYTHON_GLOB_RESULT 0 PYTHON_GLOB_RESULT)
             endif()
         endif()
 
