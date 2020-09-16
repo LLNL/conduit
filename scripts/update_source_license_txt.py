313685ea40abcd1350374dab570e1dafb503d34b
Cyrus Harrison <cyrush@llnl.gov>
2020-09-16 11:07:11 -0700

    move to use simplifed copyright stmt in source headers

--- scripts/update_source_license_txt.py
+++ scripts/update_source_license_txt.py
@@ -1,46 +1,6 @@
-###############################################################################
-# Copyright (c) 2014-2018, Lawrence Livermore National Security, LLC.
-# 
-# Produced at the Lawrence Livermore National Laboratory
-# 
-# LLNL-CODE-666778
-# 
-# All rights reserved.
-# 
-# This file is part of Conduit. 
-# 
-# For details, see: http://software.llnl.gov/conduit/.
-# 
-# Please also read conduit/LICENSE
-# 
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions are met:
-# 
-# * Redistributions of source code must retain the above copyright notice, 
-#   this list of conditions and the disclaimer below.
-# 
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the disclaimer (as noted below) in the
-#   documentation and/or other materials provided with the distribution.
-# 
-# * Neither the name of the LLNS/LLNL nor the names of its contributors may
-#   be used to endorse or promote products derived from this software without
-#   specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL SECURITY,
-# LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-# DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-# POSSIBILITY OF SUCH DAMAGE.
-# 
-###############################################################################
+# Copyright (c) Lawrence Livermore National Security, LLC and other Conduit
+# Project developers. See top-level LICENSE AND COPYRIGHT files for dates and
+# other details. No copyright assignment is required to contribute to Conduit.
 """
 file: update_source_license_txt.py
 description:
@@ -98,7 +58,7 @@ def update_lic(lic_file_old,lic_file_new,modify_files):
     for k,v in patterns.items():
         v["full"] = gen_formatted(old_lic_txt,
                                   v["hdr"][1:],
-                                  v["st"])    
+                                  v["st"])
     all_files = []
     for root_path in ["../src",
                       "../scripts",
@@ -114,31 +74,34 @@ def update_lic(lic_file_old,lic_file_new,modify_files):
                     all_files.append(full)
         else:
             all_files.append(os.path.abspath(root_path))
-    print all_files
+    for f in all_files:
+        print f
+    #sys.exit(0)
     up_cnt    = {}
     updated   = []
     for f in all_files:
-        curr_txt =  open(f).read()
-        for k,v in patterns.items():
-            full_old_lic = v["full"]
-            cnt = curr_txt.count(full_old_lic)
-            if cnt > 0:
-                print "[begin update to %s (style = %s)]" % (f,k)
-                print "[old - %s]" % f
-                print curr_txt
-                new_txt = curr_txt.replace(full_old_lic,
-                                           gen_formatted(lic_short_txt,
-                                                         v["hdr"][1:],
-                                                         v["st"]))
-                print "[new - %s]" % f
-                print new_txt
-                print "[end update to %s]" % f
-                updated.append(f)
-                if not f in up_cnt.keys():
-                    up_cnt[f] = 0
-                up_cnt[f] += 1
-                if modify_files:
-                    open(f,"w").write(new_txt)
+        if not f.endswith(".tar") and  not f.endswith("gz"):
+            curr_txt =  open(f).read()
+            for k,v in patterns.items():
+                full_old_lic = v["full"]
+                cnt = curr_txt.count(full_old_lic)
+                if cnt > 0:
+                    print "[begin update to %s (style = %s)]" % (f,k)
+                    print "[old - %s]" % f
+                    print curr_txt
+                    new_txt = curr_txt.replace(full_old_lic,
+                                               gen_formatted(lic_short_txt,
+                                                             "",
+                                                             v["st"]))
+                    print "[new - %s]" % f
+                    print new_txt
+                    print "[end update to %s]" % f
+                    updated.append(f)
+                    if not f in up_cnt.keys():
+                        up_cnt[f] = 0
+                    up_cnt[f] += 1
+                    if modify_files:
+                        open(f,"w").write(new_txt)
     print updated, len(updated)
     for k,v in up_cnt.items():
         print k,v
