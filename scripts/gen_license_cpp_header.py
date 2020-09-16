9b26529e7b33236013ce8e5524a73be7d365c1d8
Cyrus Harrison <cyrush@llnl.gov>
2020-09-16 10:10:10 -0700

    start copyright update process

--- scripts/gen_license_cpp_header.py
+++ scripts/gen_license_cpp_header.py
@@ -58,14 +58,13 @@ pattern = {
     "st": "// "}
 
 
-def gen_lic_hpp(lic_file,hpp_out):
+def gen_lic_hpp(lic_file,cpyr_hdr_file,hpp_out):
     lic_txt = open(lic_file).readlines()
+    cpry_hdr_txt = open(cpyr_hdr_file).readlines()
     # write the lic prelude, then create var to use in c++
     hpp_f = open(hpp_out,"w")
-    hpp_f.write(pattern["hdr"])
-    for l in lic_txt:
+    for l in cpry_hdr_txt:
         hpp_f.write("%s%s" % (pattern["st"],l))
-    hpp_f.write(pattern["hdr"])
     hpp_f.write("\n")
     hpp_f.write("#ifndef CONDUIT_LICENSE_TEXT_HPP\n")
     hpp_f.write("#define CONDUIT_LICENSE_TEXT_HPP\n\n")
@@ -80,11 +79,12 @@ def gen_lic_hpp(lic_file,hpp_out):
 if __name__ == "__main__":
     nargs = len(sys.argv)
     modify_files = False
-    if nargs < 3:
+    if nargs < 4:
         print "usage: python generate_cpp_license_header.py "
-        print "[new lic] [output file]"
+        print "[new lic] [lic header] [output file]"
         sys.exit(-1)
     lic_file = sys.argv[1]
-    hpp_out  = sys.argv[2]
-    gen_lic_hpp(lic_file,hpp_out)
+    cpyr_hdr = sys.argv[2]
+    hpp_out  = sys.argv[3]
+    gen_lic_hpp(lic_file,cpyr_hdr,hpp_out)
 
