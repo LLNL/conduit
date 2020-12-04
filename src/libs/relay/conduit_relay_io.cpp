2308e2637f54bede07db6517595fed237f2c16e0
Cyrus Harrison <cyrush@llnl.gov>
2020-12-04 14:14:48 -0800

    wire up defs and abouts details for zfp and h5z-zfp

--- src/libs/relay/conduit_relay_io.cpp
+++ src/libs/relay/conduit_relay_io.cpp
@@ -93,7 +93,13 @@ about(Node &n)
     io_protos["hdf5"] = "disabled";
     io_protos["sidre_hdf5"] = "enabled";
 #endif
-    
+
+#ifdef CONDUIT_RELAY_IO_H5ZZFP_ENABLED
+    io_protos["h5z-zfp"] = "enabled";
+#else
+    io_protos["h5z-zfp"] = "disabled";
+#endif
+
     // silo
 #ifdef CONDUIT_RELAY_IO_SILO_ENABLED
     // node is packed into two silo objects
