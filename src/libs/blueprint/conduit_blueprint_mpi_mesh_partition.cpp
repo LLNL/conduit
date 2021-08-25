de7d6f0e151b782116e42f6a088f68da55949acc
Brad Whitlock <bjw@ilight.com>
2021-08-25 14:43:41 -0700

    Trying a sendrecv approach.

--- src/libs/blueprint/conduit_blueprint_mpi_mesh_partition.cpp
+++ src/libs/blueprint/conduit_blueprint_mpi_mesh_partition.cpp
@@ -24,8 +24,8 @@ using std::cout;
 using std::endl;
 
 // Uncomment these macros to enable debugging output.
-//#define CONDUIT_DEBUG_MAP_CHUNKS
-//#define CONDUIT_DEBUG_COMMUNICATE_CHUNKS
+#define CONDUIT_DEBUG_MAP_CHUNKS
+#define CONDUIT_DEBUG_COMMUNICATE_CHUNKS
 
 // Renumber domains in parallel
 #define RENUMBER_DOMAINS
@@ -326,7 +326,9 @@ parallel_partitioner::map_chunks(const std::vector<partitioner::chunk> &chunks,
     for(int i = 0; i < ntotal_chunks; i++)
     {
         int domid = global_chunk_info[i].destination_domain;
-        if(domid >= 0)
+        if(domid == selection::FREE_DOMAIN_ID)
+            free_to_move++;
+        else
         {
             std::map<int,int>::iterator it = domain_sizes.find(domid);
             if(it == domain_sizes.end())
@@ -334,8 +336,6 @@ parallel_partitioner::map_chunks(const std::vector<partitioner::chunk> &chunks,
             else
                 it->second++;
         }
-        else
-            free_to_move++;
     }
 
 #ifdef CONDUIT_DEBUG_MAP_CHUNKS
@@ -345,7 +345,7 @@ parallel_partitioner::map_chunks(const std::vector<partitioner::chunk> &chunks,
         for(std::map<int,int>::const_iterator it = domain_sizes.begin();
             it != domain_sizes.end(); it++)
         {
-            cout << it->first << ": " << it->second << ", ";
+            cout << it->first << ":" << it->second << ", ";
         }
         cout << "}" << endl;
         cout << "free_to_move = " << free_to_move << endl;
@@ -354,8 +354,8 @@ parallel_partitioner::map_chunks(const std::vector<partitioner::chunk> &chunks,
 
     if(free_to_move == 0)
     {
-        // No chunks are free to move around. This means we the domains
-        // that we want them all to belong to.
+        // No chunks are free to move around into domains. We may still need
+        // to assign domains to ranks though.
 
         // NOTE: This may mean that we do not get #target domains though.
         if(!domain_sizes.empty() && domain_sizes.size() != target)
@@ -375,6 +375,56 @@ parallel_partitioner::map_chunks(const std::vector<partitioner::chunk> &chunks,
             dest_domain.push_back(global_chunk_info[i].destination_domain);
         }
         _offsets.swap(offsets);
+
+        // Take a look at the dest_rank values to see if there are domains
+        // for which we need to assign to ranks.
+        std::set<int> domains_to_assign;
+        std::map<int,int> domain_elem_counts, rank_elem_counts;
+        for(int i = 0; i < size; i++)
+            rank_elem_counts[i] = 0;
+        for(int i = 0; i < ntotal_chunks; i++)
+        {
+            if(dest_rank[i] == selection::FREE_RANK_ID)
+            {
+                // This domain is not assigned to a rank.                
+                domains_to_assign.insert(dest_domain[i]);
+            }
+            else
+            {
+                // Add the cells to the known rank.
+                rank_elem_counts[dest_rank[i]] += global_chunk_info[i].num_elements;
+            }
+            
+            // Help determine the overall element count for the domain.
+            std::map<int,int>::iterator it = domain_elem_counts.find(dest_domain[i]);
+            if(it == domain_elem_counts.end())
+                domain_elem_counts[dest_domain[i]] = global_chunk_info[i].num_elements;
+            else
+                it->second += global_chunk_info[i].num_elements;
+        }
+        // Assign domains that do not have a dest_rank.
+        if(!domains_to_assign.empty())
+        {
+            for(auto domid : domains_to_assign)
+            {
+                // Find the rank that has the least cells.
+                std::map<int,int>::iterator it, rit;
+                rit = rank_elem_counts.begin();
+                for(it = rank_elem_counts.begin(); it != rank_elem_counts.end(); it++)
+                {
+                    if(it->second < rit->second)
+                        rit = it;
+                }
+                // Now we know which rank will get the domain. Record it
+                // into dest_rank.
+                rit->second += domain_elem_counts[domid];
+                for(int i = 0; i < ntotal_chunks; i++)
+                {
+                    if(dest_domain[i] == domid)
+                        dest_rank[i] = rit->first;
+                }
+            }
+        }
     }
     else if(free_to_move == ntotal_chunks)
     {
@@ -392,7 +442,7 @@ parallel_partitioner::map_chunks(const std::vector<partitioner::chunk> &chunks,
 
         std::vector<uint64> target_element_counts(target, 0);
         std::vector<uint64> next_target_element_counts(target);
-        std::vector<int> global_dest_rank(ntotal_chunks, -1);
+        std::vector<int> global_dest_rank(ntotal_chunks, selection::FREE_RANK_ID);
         std::vector<int> global_dest_domain(ntotal_chunks, 0);
         for(int i = 0; i < ntotal_chunks; i++)
         {
@@ -566,6 +616,160 @@ parallel_partitioner::communicate_chunks(const std::vector<partitioner::chunk> &
     MPI_Barrier(comm);
 #endif
 
+#if 1
+//------------------------------------------------------------------------------------------------------
+    // Get the indices of the sends, recvs that we'll do.
+    std::vector<int> sends, recvs;
+    for(size_t i = 0; i < chunks.size(); i++)
+    {
+        int gidx = offsets[rank] + i;
+        int tag = PARTITION_TAG_BASE + gidx;
+        int dest = dest_rank[gidx];
+        if(dest != rank)
+        {
+            sends.push_back(i);
+        }
+    }
+    for(size_t i = 0; i < dest_rank.size(); i++)
+    {
+        if(dest_rank[i] == rank)
+        {
+            int gidx = i;
+            int tag = PARTITION_TAG_BASE + gidx;
+            int start = offsets[rank];
+            int end = start + chunks.size();
+            bool this_rank_already_owns_it = (gidx >= start && gidx < end);
+            if(!this_rank_already_owns_it)
+            {
+                recvs.push_back(i);
+            }
+        }
+    }
+
+    // Do a certain number of the sends/recvs as a send_recv to help avoid deadlock.
+    int si = 0;
+    int ri = 0;
+    if(sends.size() > 0 && recvs.size() > 0)
+    {
+        size_t max_pairs = std::max(sends.size(), recvs.size());
+        for(size_t i = 0; i < max_pairs; i++)
+        {
+            // Compute send info.
+            int send_gidx = offsets[rank] + sends[si];
+            int sendtag = PARTITION_TAG_BASE + send_gidx;
+            int dest = dest_rank[send_gidx];
+
+            // Compute recv info.
+            int recv_gidx = recvs[ri];
+            int recvtag = PARTITION_TAG_BASE + recv_gidx;
+            int src = src_rank[recvs[ri]];
+
+#ifdef CONDUIT_DEBUG_COMMUNICATE_CHUNKS
+            cout << rank << ": send_recv_using_schema(dest=" << dest << ", sendtag=" << sendtag
+                         << ", src=" << src << ", recvtag=" << recvtag << ")" << endl;
+#endif
+            conduit::Node *n_recv = new conduit::Node;
+//            conduit::relay::mpi::send_recv_using_schema(*chunks[sends[si]].mesh, dest, sendtag,
+//                                                        *n_recv, src, recvtag,
+//                                                        comm);
+
+#ifdef RENUMBER_DOMAINS
+            // Since we had to receive the chunk, we can patch up its state/domain_id
+            // to the updated numbering scheme.
+            (*n_recv)["state/domain_id"] = recvs[ri];
+#endif
+            // Save the received chunk and indicate we own it for later.
+            chunks_to_assemble.push_back(chunk(n_recv, true));
+            chunks_to_assemble_domains.push_back(dest_domain[recvs[ri]]);
+
+            si++; ri++;
+        }
+    }
+
+    // Do remaining sends.
+    for(; si < sends.size(); si++)
+    {
+        int gidx = offsets[rank] + sends[si];
+        int tag = PARTITION_TAG_BASE + gidx;
+        int dest = dest_rank[gidx];
+        
+#ifdef CONDUIT_DEBUG_COMMUNICATE_CHUNKS
+        cout << rank << ": send_using_schema(dest=" << dest
+             << ", tag=" << tag << ")" << endl;
+#endif
+//        conduit::relay::mpi::send_using_schema(*chunks[sends[si]].mesh, dest, tag, comm);
+    }
+
+    // Do remaining recvs.
+    for(; ri < recvs.size(); ri++)
+    {
+        int i = recvs[ri];
+        int gidx = i;
+        int tag = PARTITION_TAG_BASE + gidx;
+#ifdef CONDUIT_DEBUG_COMMUNICATE_CHUNKS
+        cout << rank << ": recv_using_schema(src=" << src_rank[i]
+             << ", tag=" << tag << ")" << endl;
+#endif
+        conduit::Node *n_recv = new conduit::Node;
+//        conduit::relay::mpi::recv_using_schema(*n_recv, src_rank[i], tag, comm);
+
+#ifdef RENUMBER_DOMAINS
+        // Since we had to receive the chunk, we can patch up its state/domain_id
+        // to the updated numbering scheme.
+        (*n_recv)["state/domain_id"] = i;
+#endif
+        // Save the received chunk and indicate we own it for later.
+        chunks_to_assemble.push_back(chunk(n_recv, true));
+        chunks_to_assemble_domains.push_back(dest_domain[i]);
+    }
+
+    // Do all the local recvs where we already owned the data and did not
+    // have to move it.
+    for(size_t i = 0; i < dest_rank.size(); i++)
+    {
+        if(dest_rank[i] == rank)
+        {
+            int gidx = i;
+            int tag = PARTITION_TAG_BASE + gidx;
+            int start = offsets[rank];
+            int end = start + chunks.size();
+            bool this_rank_already_owns_it = (gidx >= start && gidx < end);
+            if(this_rank_already_owns_it)
+            {
+                int local_i = i - offsets[rank];
+
+#ifdef RENUMBER_DOMAINS
+                // The chunk we have here needs its state/domain_id updated but
+                // we really should not modify it directly. Do we have to make
+                // a new node and set_external everything in it? Then make it
+                // have its own state/domain_id?
+                conduit::Node *n_recv = new conduit::Node;
+                for(index_t ci = 0; ci < chunks[local_i].mesh->number_of_children(); ci++)
+                {
+                    const conduit::Node &n = chunks[local_i].mesh->operator[](ci);
+                    if(n.name() != "state")
+                        (*n_recv)[n.name()].set_external_node(n);
+                }
+                if(chunks[local_i].mesh->has_path("state/cycle"))
+                    (*n_recv)["state/cycle"] = (*chunks[local_i].mesh)["state/cycle"];
+                if(chunks[local_i].mesh->has_path("state/time"))
+                    (*n_recv)["state/time"] = (*chunks[local_i].mesh)["state/time"];
+                (*n_recv)["state/domain_id"] = i;
+
+                // Save the chunk "wrapper" that has its own state.
+                chunks_to_assemble.push_back(chunk(n_recv, true));
+                chunks_to_assemble_domains.push_back(dest_domain[i]);
+#else
+                // Pass the chunk through since we already own it on this rank.
+                chunks_to_assemble.push_back(chunk(chunks[local_i].mesh, false));
+                chunks_to_assemble_domains.push_back(dest_domain[i]);
+#endif
+            }
+        }
+    }
+
+#else
+//------------------------------------------------------------------------------------------------------
     // Do sends for the chunks we own on this processor that must migrate.
     for(size_t i = 0; i < chunks.size(); i++)
     {
@@ -579,7 +783,7 @@ parallel_partitioner::communicate_chunks(const std::vector<partitioner::chunk> &
             cout << rank << ": send_using_schema(dest=" << dest
                  << ", tag=" << tag << ")" << endl;
 #endif
-            conduit::relay::mpi::send_using_schema(*chunks[i].mesh, dest, tag, comm);
+//            conduit::relay::mpi::send_using_schema(*chunks[i].mesh, dest, tag, comm);
         }
     }
 
@@ -631,7 +835,7 @@ parallel_partitioner::communicate_chunks(const std::vector<partitioner::chunk> &
                      << ", tag=" << tag << ")" << endl;
 #endif
                 conduit::Node *n_recv = new conduit::Node;              
-                conduit::relay::mpi::recv_using_schema(*n_recv, src_rank[i], tag, comm);
+//                conduit::relay::mpi::recv_using_schema(*n_recv, src_rank[i], tag, comm);
 
 #ifdef RENUMBER_DOMAINS
                 // Since we had to receive the chunk, we can patch up its state/domain_id
@@ -644,6 +848,7 @@ parallel_partitioner::communicate_chunks(const std::vector<partitioner::chunk> &
             }
         }
     }
+#endif
 }
 
 //-----------------------------------------------------------------------------
