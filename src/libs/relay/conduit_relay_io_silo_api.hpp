5f7ee42e692e8e48d447a9cf1da328c7f2704910
James Corbett <corbett8@llnl.gov>
2021-09-01 12:07:34 -0700

    silo: declare api functions

--- src/libs/relay/conduit_relay_io_silo_api.hpp
+++ src/libs/relay/conduit_relay_io_silo_api.hpp
@@ -39,7 +39,7 @@ void CONDUIT_RELAY_API silo_read(DBfile *dbfile,
                                  Node &node);
 
 
-//-----------------------------------------------------------------------------    
+//-----------------------------------------------------------------------------
 void CONDUIT_RELAY_API silo_mesh_write(const Node &mesh,
                                        const std::string &path);
 
@@ -52,5 +52,104 @@ void CONDUIT_RELAY_API silo_mesh_write(const Node &mesh,
 void CONDUIT_RELAY_API silo_mesh_write(const Node &mesh,
                                        DBfile *dbfile,
                                        const std::string &silo_obj_path);
+
+//-----------------------------------------------------------------------------
+// -- begin <>::silo --
+//-----------------------------------------------------------------------------
+namespace silo
+{
+
+//-----------------------------------------------------------------------------
+// Save a blueprint mesh to silo
+//-----------------------------------------------------------------------------
+/// These methods assume `mesh` is a valid blueprint mesh.
+///
+/// Note: These methods use "save" semantics, they will overwrite existing
+///       files.
+///
+///
+//-----------------------------------------------------------------------------
+void CONDUIT_RELAY_API save_mesh(const conduit::Node &mesh,
+                                 const std::string &path);
+
+//-----------------------------------------------------------------------------
+/// The following options can be passed via the opts Node:
+//-----------------------------------------------------------------------------
+/// opts:
+///      TODO
+///
+//-----------------------------------------------------------------------------
+void CONDUIT_RELAY_API save_mesh(const conduit::Node &mesh,
+                                 const std::string &path,
+                                 const conduit::Node &opts);
+
+//-----------------------------------------------------------------------------
+// Write a blueprint mesh to silo
+//-----------------------------------------------------------------------------
+/// These methods assume `mesh` is a valid blueprint mesh.
+///
+/// Note: These methods use "write" semantics, they will append to existing
+///       files.
+///
+///
+//-----------------------------------------------------------------------------
+void CONDUIT_RELAY_API write_mesh(const conduit::Node &mesh,
+                                  const std::string &path);
+
+//-----------------------------------------------------------------------------
+/// The following options can be passed via the opts Node:
+//-----------------------------------------------------------------------------
+/// opts:
+///      TODO
+///
+//-----------------------------------------------------------------------------
+void CONDUIT_RELAY_API write_mesh(const conduit::Node &mesh,
+                                  const std::string &path,
+                                  const conduit::Node &opts);
+
+
+//-----------------------------------------------------------------------------
+// Load a blueprint mesh from root + file set
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+void CONDUIT_RELAY_API read_mesh(const std::string &root_file_path,
+                                 conduit::Node &mesh);
+
+
+//-----------------------------------------------------------------------------
+///
+/// opts:
+///      TODO
+///
+//-----------------------------------------------------------------------------
+void CONDUIT_RELAY_API read_mesh(const std::string &root_file_path,
+                                 const conduit::Node &opts,
+                                 conduit::Node &mesh);
+
+
+//-----------------------------------------------------------------------------
+// The load semantics, the mesh node is reset before reading.
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+void CONDUIT_RELAY_API load_mesh(const std::string &root_file_path,
+                                 conduit::Node &mesh);
+
+
+//-----------------------------------------------------------------------------
+///
+/// opts:
+///      TODO
+///
+//-----------------------------------------------------------------------------
+void CONDUIT_RELAY_API load_mesh(const std::string &root_file_path,
+                                 const conduit::Node &opts,
+                                 conduit::Node &mesh);
+
+//-----------------------------------------------------------------------------
+// -- end <>::silo --
+//-----------------------------------------------------------------------------
+}
 
 #endif
