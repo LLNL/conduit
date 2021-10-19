e4728d96eec6f42247a1f193e7d0444d5d0cabf4
Max Yang <yang39@llnl.gov>
2021-10-19 12:34:33 -0700

    Add test for uniform mesh with adjsets

--- src/tests/blueprint/t_blueprint_mpi_mesh_parmetis.cpp
+++ src/tests/blueprint/t_blueprint_mpi_mesh_parmetis.cpp
@@ -180,6 +180,99 @@ TEST(blueprint_mpi_parmetis, braid)
 }
 
 //-----------------------------------------------------------------------------
+TEST(blueprint_mpi_parmetis, uniform_adjset)
+{
+    int par_size, par_rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &par_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &par_rank);
+
+    const int npts = 10;
+
+    // test with a 2d poly example
+    Node mesh, side_mesh, info;
+
+    // create uniform mesh with adjsets
+    conduit::blueprint::mesh::examples::adjset_uniform(mesh);
+
+    // extract local domains
+    Node local_mesh;
+    for (int i = 0; i < 8; i++)
+    {
+        if (i % par_size != par_rank)
+        {
+            continue;
+        }
+        std::ostringstream oss;
+        oss << "domain_" << std::setfill('0') << std::setw(6) << i;
+        const std::string domain_name = oss.str();
+        local_mesh[domain_name] = mesh[domain_name];
+    }
+
+    // construct unstructured topology
+    for (Node& domain : local_mesh.children())
+    {
+        Node unstruct_topo, unstruct_coords;
+        conduit::blueprint::mesh::topology::uniform::to_unstructured(domain["topologies/topo"],
+                                                                     unstruct_topo,
+                                                                     unstruct_coords);
+        Node unstruct_topo_poly;
+        conduit::blueprint::mesh::topology::unstructured::to_polygonal(unstruct_topo, unstruct_topo_poly);
+        domain["topologies/topo"] = unstruct_topo_poly;
+        domain["topologies/topo/coordset"] = "coords";
+        domain["coordsets/coords"] = unstruct_coords;
+    }
+
+    EXPECT_TRUE(conduit::blueprint::mesh::verify(local_mesh, info));
+
+    // paint a field with parmetis result (WIP)
+    conduit::blueprint::mpi::mesh::generate_partition_field(local_mesh,MPI_COMM_WORLD);
+
+    for (int i = 0; i < 8; i++)
+    {
+        if (i % par_size != par_rank)
+        {
+            continue;
+        }
+        std::ostringstream oss;
+        oss << "domain_" << std::setfill('0') << std::setw(6) << i;
+        const std::string domain_name = oss.str();
+
+        const Node& in_domain = local_mesh[domain_name];
+        Node& side_mesh_dom = side_mesh[domain_name];
+
+        Node s2dmap, d2smap;
+        Node &side_coords = side_mesh_dom["coordsets/coords"];
+        Node &side_topo = side_mesh_dom["topologies/topo"];
+        Node &side_fields = side_mesh_dom["fields"];
+
+        // we can't map vert assoced fields yet
+        Node opts;
+        opts["field_names"].append().set("global_element_ids");
+        opts["field_names"].append().set("parmetis_result");
+
+        // gen sides and save so we can look at this in visit.
+        blueprint::mesh::topology::unstructured::generate_sides(in_domain["topologies/topo"],
+                                                                side_topo,
+                                                                side_coords,
+                                                                side_fields,
+                                                                s2dmap,
+                                                                d2smap,
+                                                                opts);
+    }
+    std::string output_base = "tout_bp_mpi_mesh_parametis_uniform_adjset2d_test";
+
+    // Node opts;
+    // opts["file_style"] = "root_only";
+    conduit::relay::mpi::io::blueprint::save_mesh(side_mesh,
+            output_base,
+            "hdf5",
+            // opts,
+            MPI_COMM_WORLD);
+    EXPECT_TRUE(true);
+}
+
+//-----------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
     int result = 0;
