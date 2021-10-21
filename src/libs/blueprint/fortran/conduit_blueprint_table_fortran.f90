3ee3e9d336ce408b8a78b3e3d3699e18e80b4c52
Christopher Laganella <cdl@ilight.com>
2021-10-21 18:51:13 -0400

    Follow pattern used in other fortran bindings

--- src/libs/blueprint/fortran/conduit_blueprint_table_fortran.f90
+++ src/libs/blueprint/fortran/conduit_blueprint_table_fortran.f90
@@ -18,17 +18,17 @@ module conduit_blueprint_table
     !--------------------------------------------------------------------------
 
         !--------------------------------------------------------------------------
-        function conduit_blueprint_table_verify(cnode, cinfo) result(res) &
+        function c_conduit_blueprint_table_verify(cnode, cinfo) result(res) &
                 bind(C, name="conduit_blueprint_table_verify")
             use iso_c_binding
             implicit none
             type(C_PTR), value, intent(IN) :: cnode
             type(C_PTR), value, intent(IN) :: cinfo
             logical(C_BOOL) ::res
-        end function conduit_blueprint_table_verify
+        end function c_conduit_blueprint_table_verify
 
         !--------------------------------------------------------------------------
-        function conduit_blueprint_table_verify_sub_protocol(protocol, cnode, cinfo) result(res) &
+        function c_conduit_blueprint_table_verify_sub_protocol(protocol, cnode, cinfo) result(res) &
                 bind(C, name="conduit_blueprint_table_verify_sub_protocol")
             use iso_c_binding
             implicit none
@@ -36,10 +36,10 @@ module conduit_blueprint_table
             type(C_PTR), value, intent(IN) :: cnode
             type(C_PTR), value, intent(IN) :: cinfo
             logical(C_BOOL) ::res
-        end function conduit_blueprint_table_verify_sub_protocol
+        end function c_conduit_blueprint_table_verify_sub_protocol
 
         !--------------------------------------------------------------------------
-        subroutine conduit_blueprint_table_examples_basic(nx, ny, nz, cnode) &
+        subroutine c_conduit_blueprint_table_examples_basic(nx, ny, nz, cnode) &
                 bind(C, name="conduit_blueprint_table_examples_basic")
             use iso_c_binding
             implicit none
@@ -47,11 +47,52 @@ module conduit_blueprint_table
             integer(C_SIZE_T), value, intent(in) :: ny
             integer(C_SIZE_T), value, intent(in) :: nz
             type(C_PTR), value, intent(IN) :: cnode
-        end subroutine conduit_blueprint_table_examples_basic
+        end subroutine c_conduit_blueprint_table_examples_basic
 
     !--------------------------------------------------------------------------
     end interface
     !--------------------------------------------------------------------------
+
+    !--------------------------------------------------------------------------
+    contains
+    !--------------------------------------------------------------------------
+
+        !--------------------------------------------------------------------------
+        function conduit_blueprint_table_verify(cnode, cinfo) result(res)
+            use iso_c_binding
+            implicit none
+            type(C_PTR), value, intent(IN) :: cnode
+            type(C_PTR), value, intent(IN) :: cinfo
+            logical(C_BOOL) ::res
+            !---
+            res = c_conduit_blueprint_table_verify(cnode, cinfo)
+        end function conduit_blueprint_table_verify
+
+        !--------------------------------------------------------------------------
+        function conduit_blueprint_table_verify_sub_protocol(protocol, cnode, cinfo) result(res)
+            use iso_c_binding
+            implicit none
+            character(*), intent(IN) :: protocol
+            type(C_PTR), value, intent(IN) :: cnode
+            type(C_PTR), value, intent(IN) :: cinfo
+            logical(C_BOOL) ::res
+            !---
+            res = c_conduit_blueprint_table_verify_sub_protocol(trim(protocol) // C_NULL_CHAR, &
+                                                                cnode, &
+                                                                cinfo)
+        end function conduit_blueprint_table_verify_sub_protocol
+
+        !--------------------------------------------------------------------------
+        subroutine conduit_blueprint_table_examples_basic(nx, ny, nz, cnode)
+            use iso_c_binding
+            implicit none
+            integer(C_SIZE_T), value, intent(in) :: nx
+            integer(C_SIZE_T), value, intent(in) :: ny
+            integer(C_SIZE_T), value, intent(in) :: nz
+            type(C_PTR), value, intent(IN) :: cnode
+            !---
+            call c_conduit_blueprint_table_examples_basic(nx, ny, nz, cnode)
+        end subroutine conduit_blueprint_table_examples_basic
 
 !------------------------------------------------------------------------------
 end module conduit_blueprint_table
