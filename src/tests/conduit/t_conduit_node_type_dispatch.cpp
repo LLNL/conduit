de3a873068ba971405cfc003d3286673f2cedfcd
Cyrus Harrison <cyrush@llnl.gov>
2022-09-05 14:21:18 -0700

    add timing diagnostics to hdf5 opts test (#1016)
    * add timer util 
    * add timer diag to hdf5 opts test

--- src/tests/conduit/t_conduit_node_type_dispatch.cpp
+++ src/tests/conduit/t_conduit_node_type_dispatch.cpp
@@ -11,46 +11,14 @@
 #include "conduit.hpp"
 
 #include <iostream>
-#include <chrono>
-
 #include "gtest/gtest.h"
 #include "rapidjson/document.h"
 using namespace conduit;
+using namespace conduit::utils;
 
 
 index_t ARRAY_SIZE = 1000000;
 index_t NUM_ITERS  = 1;
-
-
-/// simple timer class
-//-----------------------------------------------------------------------------
-class Timer 
-{
-
-  typedef std::chrono::high_resolution_clock high_resolution_clock;
-  typedef std::chrono::nanoseconds nanoseconds;
-  typedef std::chrono::duration<float> fsec;
-
-public:
-    explicit Timer()
-    {
-      reset();
-    }
-
-    void reset()
-    {
-      m_start = high_resolution_clock::now();
-    }
-
-    float elapsed() const
-    {
-       auto ftime  = std::chrono::duration_cast<fsec>(high_resolution_clock::now() - m_start);
-       return ftime.count();
-    }
-
-private:
-    high_resolution_clock::time_point m_start;
-};
 
 
 //-----------------------------------------------------------------------------
