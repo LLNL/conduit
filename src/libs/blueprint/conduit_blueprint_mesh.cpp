94e80a4f2965094a76cfe9e4adaa3a341273299f
Justin Privitera <privitera1@llnl.gov>
2023-07-14 11:24:58 -0700

    change u to t

--- src/libs/blueprint/conduit_blueprint_mesh.cpp
+++ src/libs/blueprint/conduit_blueprint_mesh.cpp
@@ -5075,20 +5075,20 @@ namespace detail
         }
     }
 
-    template<typename U> // U is the type of the new field values
+    template<typename T> // T is the type of the new field values
     void
     vertex_associated_field(const Node &topo_dest,
                             float_accessor poly_field_data,
                             int orig_num_points,
                             int new_num_points,
                             int dimensions,
-                            U *values_array)
+                            T *values_array)
     {
         // copy field values from the original field over to the
         // points that are in both the old and new topologies
         for (int i = 0; i < orig_num_points; i ++)
         {
-            values_array[i] = static_cast<U>(poly_field_data[i]);
+            values_array[i] = static_cast<T>(poly_field_data[i]);
         }
 
         // this map will record for each new point (represented by
@@ -5155,17 +5155,17 @@ namespace detail
                 // the size of the set, since there are neighbors which
                 // may go unused, since they are not from the original
                 // coordset
-                values_array[i] = static_cast<U>(sum / num_neighbors);
+                values_array[i] = static_cast<T>(sum / num_neighbors);
             }
             // if the points go unused in the topology, we assign them 0
             else
             {
-                values_array[i] = static_cast<U>(0.0);
+                values_array[i] = static_cast<T>(0.0);
             }
         }
     }
 
-    template<typename U> // U is the type of the new field values
+    template<typename T> // T is the type of the new field values
     void
     map_field_to_generated_sides(Node &field_out,
                                  const Node &field_src,
@@ -5180,7 +5180,7 @@ namespace detail
                                  const Node &topo_dest)
     {
         // a pointer to the destination for field values
-        U *values_array = field_out["values"].value();
+        T *values_array = field_out["values"].value();
 
         // a pointer to the original field values
         float_accessor poly_field_data = field_src["values"].value();
@@ -5188,7 +5188,7 @@ namespace detail
         // if our field is vertex associated
         if (vert_assoc)
         {
-            vertex_associated_field<U>(topo_dest,
+            vertex_associated_field<T>(topo_dest,
                                        poly_field_data,
                                        orig_num_points,
                                        new_num_points,
@@ -5208,11 +5208,11 @@ namespace detail
                 // if our field is volume dependent
                 if (vol_dep)
                 {
-                    values_array[i] = static_cast<U>(poly_field_data[tri_to_poly[i]] * volume_ratio[i]);
+                    values_array[i] = static_cast<T>(poly_field_data[tri_to_poly[i]] * volume_ratio[i]);
                 }
                 else
                 {
-                    values_array[i] = static_cast<U>(poly_field_data[tri_to_poly[i]]);
+                    values_array[i] = static_cast<T>(poly_field_data[tri_to_poly[i]]);
                 }
             }
         }
