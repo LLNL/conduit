209b4426d322b0503446408556bfc364c14e7f31
Cyrus Harrison <cyrush@llnl.gov>
2023-11-07 09:20:43 -0800

    updates
    index 53ed31e5a..a900e48f9 100644

--- src/tests/thirdparty/t_zlib_smoke.cpp
+++ src/tests/thirdparty/t_zlib_smoke.cpp
@@ -1,8 +1,6 @@
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-// Copyright (c) Lawrence Livermore National Security, LLC and other Ascent
+// Copyright (c) Lawrence Livermore National Security, LLC and other Conduit
 // Project developers. See top-level LICENSE AND COPYRIGHT files for dates and
-// other details. No copyright assignment is required to contribute to Ascent.
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// other details. No copyright assignment is required to contribute to Conduit.
 
 //-----------------------------------------------------------------------------
 ///
