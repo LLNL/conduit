04c5b577b941d6192b7dc34704d2e7d96ec78010
Justin Privitera <privitera1@llnl.gov>
2024-01-10 17:27:23 -0800

    sbe to full

--- src/libs/blueprint/conduit_blueprint_mesh_matset_xforms.cpp
+++ src/libs/blueprint/conduit_blueprint_mesh_matset_xforms.cpp
@@ -560,6 +560,63 @@ full_to_sparse_by_element(const conduit::Node &matset,
 
 //-----------------------------------------------------------------------------
 void
+sparse_by_element_to_full(const conduit::Node &matset,
+                          conduit::Node &dest)
+{
+    // set the topology
+    dest["topology"].set(matset["topology"]);
+
+    std::map<int, std::string> reverse_matmap;
+
+    auto matmap_itr = matset["material_map"].children();
+    while (matmap_itr.has_next())
+    {
+        const Node &matmap_entry = matmap_itr.next();
+        const std::string matname = matmap_itr.name();
+
+        reverse_matmap[matmap_entry.value()] = matname;
+    }
+
+    double_accessor volume_fractions = matset["volume_fractions"].value();
+    int_accessor material_ids = matset["material_ids"].value();
+    int_accessor sizes = matset["sizes"].value();
+    int_accessor offsets = matset["offsets"].value();
+    int num_elems = sizes.dtype().number_of_elements();
+
+    std::map<std::string, std::vector<double>> vol_fracs;
+
+    for (auto & mapitem : reverse_matmap)
+    {
+        const std::string &matname = mapitem.second;
+        vol_fracs[matname] = std::vector<double>(num_elems);
+    }
+
+    for (int elem_id = 0; elem_id < num_elems; elem_id ++)
+    {
+        int size = sizes[elem_id];
+        int offset = offsets[elem_id];
+
+        for (int mat_vf_id = 0; mat_vf_id < size; mat_vf_id ++)
+        {
+            int index = offset + mat_vf_id;
+            double vol_frac = volume_fractions[index];
+            int mat_id = material_ids[index];
+            const std::string &matname = reverse_matmap[mat_id];
+            vol_fracs[matname][elem_id] = vol_frac;
+        }
+    }
+
+    for (auto & mapitem : vol_fracs)
+    {
+        const std::string &matname = mapitem.first;
+        const std::vector<double> full_vfs = mapitem.second;
+
+        dest["volume_fractions"][matname].set(full_vfs.data(), full_vfs.size());
+    }
+}
+
+//-----------------------------------------------------------------------------
+void
 full_to_sparse_by_material(const conduit::Node &matset,
                            conduit::Node &dest,
                            const float64 epsilon)
@@ -726,7 +783,7 @@ convert_matset(const conduit::Node &src_matset,
     {
         if (dest_matset_type == "full")
         {
-
+            detail::sparse_by_element_to_full(src_matset, dest_matset);
         }
         else if (dest_matset_type == "sparse_by_material")
         {
