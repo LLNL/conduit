adac0a24bf17cb3b63eb041a4ad3a135e1e8e78c
Brad Whitlock <whitlock2@llnl.gov>
2024-04-04 17:11:22 -0700

    Fix issue with MPI tags on some MPI distributions.

--- src/libs/blueprint/conduit_blueprint_mpi_mesh_parmetis.cpp
+++ src/libs/blueprint/conduit_blueprint_mpi_mesh_parmetis.cpp
@@ -464,7 +464,7 @@ void generate_global_element_and_vertex_ids(conduit::Node &mesh,
             for (const std::set<uint64>& group : recv_groups)
             {
                 index_t domid = *(group.begin());
-                const int tag = TAG_SHARED_NODE_SYNC + domid * 100 + group_idx;
+                const int tag = conduit::relay::mpi::safe_tag(TAG_SHARED_NODE_SYNC + domid * 100 + group_idx);
                 async_recvs.push_back(MPI_Request{});
                 group_idx++;
                 std::vector<uint64>& recvbuf = groups_2_vids[group];
@@ -484,7 +484,7 @@ void generate_global_element_and_vertex_ids(conduit::Node &mesh,
             for (const std::set<uint64>& group : send_groups)
             {
                 index_t domid = *(group.begin());
-                const int tag = TAG_SHARED_NODE_SYNC + domid * 100 + group_idx;
+                const int tag = conduit::relay::mpi::safe_tag(TAG_SHARED_NODE_SYNC + domid * 100 + group_idx);
                 async_sends.push_back(MPI_Request{});
                 group_idx++;
                 const std::vector<uint64>& sendbuf = groups_2_vids[group];
