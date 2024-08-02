cfe5b68a71bdef36666e4a9ff9dd7dd65de85f7c
Justin Privitera <privitera1@llnl.gov>
2024-08-01 17:33:15 -0700

    they call me mr gtest

--- src/tests/conduit/t_conduit_generator.cpp
+++ src/tests/conduit/t_conduit_generator.cpp
@@ -35,75 +35,75 @@ TEST(conduit_generator, simple_gen_schema)
         Node n;
         g1.walk(n);
         
-    //     EXPECT_EQ(n["a"].as_uint32(),a_val);
-    //     EXPECT_EQ(n["b"].as_uint32(),b_val);
-    //     EXPECT_EQ(n["c"].as_float64(),c_val);
+        EXPECT_EQ(n["a"].as_uint32(),a_val);
+        EXPECT_EQ(n["b"].as_uint32(),b_val);
+        EXPECT_EQ(n["c"].as_float64(),c_val);
     
-    //     std::string s2_str = "{\"g\": {\"a\":\"uint32\",\"b\":\"uint32\",\"c\":\"float64\"}}";
-    //     std::cout << s2_str << std::endl;
-    //     Generator g2(s2_str);
-    //     Schema schema2;
-    //     g2.walk(schema2);
+        std::string s2_str = "{\"g\": {\"a\":\"uint32\",\"b\":\"uint32\",\"c\":\"float64\"}}";
+        std::cout << s2_str << std::endl;
+        Generator g2(s2_str);
+        Schema schema2;
+        g2.walk(schema2);
         
-    //     Node n2(schema2,data,true); // true for external
-    //     EXPECT_EQ(n2["g"]["a"].as_uint32(),a_val);
-    //     EXPECT_EQ(n2["g"]["b"].as_uint32(),b_val);
-    //     EXPECT_EQ(n2["g"]["c"].as_float64(),c_val);
+        Node n2(schema2,data,true); // true for external
+        EXPECT_EQ(n2["g"]["a"].as_uint32(),a_val);
+        EXPECT_EQ(n2["g"]["b"].as_uint32(),b_val);
+        EXPECT_EQ(n2["g"]["c"].as_float64(),c_val);
     
-    //     uint32 *data2 = new uint32[5];
-    //     for (int i = 0; i < 5; i++) {
-    //        data2[i] = i * 5;
-    //     }
+        uint32 *data2 = new uint32[5];
+        for (int i = 0; i < 5; i++) {
+           data2[i] = i * 5;
+        }
     
-    //     Generator g3("{\"dtype\":\"uint32\",\"length\": 5}");
-    //     Schema schema3;
-    //     g3.walk(schema3);
-    //     Node n3(schema3,data2,true); // true for external
+        Generator g3("{\"dtype\":\"uint32\",\"length\": 5}");
+        Schema schema3;
+        g3.walk(schema3);
+        Node n3(schema3,data2,true); // true for external
     
-    //     for (int i = 0; i < 5; i++) {
-    //        EXPECT_EQ(n3.as_uint32_ptr()[i], i*5);
-    //     }
+        for (int i = 0; i < 5; i++) {
+           EXPECT_EQ(n3.as_uint32_ptr()[i], i*5);
+        }
     
-    //     Generator g4("[\"uint32\", \"float64\", \"uint32\"]");
+        Generator g4("[\"uint32\", \"float64\", \"uint32\"]");
     
-    //     char* data3 = new char[16];
-    //     memcpy(&data3[0],&a_val,4);
-    //     memcpy(&data3[4],&c_val,8);
-    //     memcpy(&data3[12],&b_val,4);
-    //     Schema schema4;
-    //     g4.walk(schema4);
-    //     Node n4(schema4,data3,true); // true for external
-    //     EXPECT_EQ(n4[0].as_uint32(),a_val);
-    //     EXPECT_EQ(n4[1].as_float64(),c_val);
-    //     EXPECT_EQ(n4[2].as_uint32(),b_val);
+        char* data3 = new char[16];
+        memcpy(&data3[0],&a_val,4);
+        memcpy(&data3[4],&c_val,8);
+        memcpy(&data3[12],&b_val,4);
+        Schema schema4;
+        g4.walk(schema4);
+        Node n4(schema4,data3,true); // true for external
+        EXPECT_EQ(n4[0].as_uint32(),a_val);
+        EXPECT_EQ(n4[1].as_float64(),c_val);
+        EXPECT_EQ(n4[2].as_uint32(),b_val);
     
-    //     Generator g5("{\"top\":[{\"int1\":\"uint32\", \"int2\":\"uint32\"}, \"float64\", \"uint32\"], \"other\":\"float64\"}");
-    //     Schema schema5;
-    //     g5.walk(schema5);
-            
-    //     char* data4 = new char[28];
-    //     uint32   d_val  = 40;
-    //     float64  e_val  = 50.0;
-    //     memcpy(&data4[0],&a_val,4);
-    //     memcpy(&data4[4],&b_val,4);
-    //     memcpy(&data4[8],&c_val,8);
-    //     memcpy(&data4[16],&d_val,4);
-    //     memcpy(&data4[20],&e_val,8);
+        Generator g5("{\"top\":[{\"int1\":\"uint32\", \"int2\":\"uint32\"}, \"float64\", \"uint32\"], \"other\":\"float64\"}");
+        Schema schema5;
+        g5.walk(schema5);
+
+        char* data4 = new char[28];
+        uint32   d_val  = 40;
+        float64  e_val  = 50.0;
+        memcpy(&data4[0],&a_val,4);
+        memcpy(&data4[4],&b_val,4);
+        memcpy(&data4[8],&c_val,8);
+        memcpy(&data4[16],&d_val,4);
+        memcpy(&data4[20],&e_val,8);
         
-    //     Node n5(schema5,data4,true); // true for external
+        Node n5(schema5,data4,true); // true for external
         
-    //     n5.schema().print();
-    //     EXPECT_EQ(n5["top"][0]["int1"].as_uint32(),a_val);
-    //     EXPECT_EQ(n5["top"][0]["int2"].as_uint32(),b_val);
-    //     EXPECT_EQ(n5["top"][1].as_float64(),c_val);
-    //     EXPECT_EQ(n5["top"][2].as_uint32(),d_val);
-    //     EXPECT_EQ(n5["other"].as_float64(),e_val);
+        n5.schema().print();
+        EXPECT_EQ(n5["top"][0]["int1"].as_uint32(),a_val);
+        EXPECT_EQ(n5["top"][0]["int2"].as_uint32(),b_val);
+        EXPECT_EQ(n5["top"][1].as_float64(),c_val);
+        EXPECT_EQ(n5["top"][2].as_uint32(),d_val);
+        EXPECT_EQ(n5["other"].as_float64(),e_val);
     
     
         delete [] data;
-    //     delete [] data2;
-    //     delete [] data3;
-    //     delete [] data4;
+        delete [] data2;
+        delete [] data3;
+        delete [] data4;
     }
 
     // YAML
@@ -138,166 +138,274 @@ TEST(conduit_generator, simple_gen_schema)
         EXPECT_EQ(n2["g"]["b"].as_uint32(),b_val);
         EXPECT_EQ(n2["g"]["c"].as_float64(),c_val);
         
-        // uint32 *data2 = new uint32[5];
-        // for (int i = 0; i < 5; i++) {
-        //    data2[i] = i * 5;
-        // }
+        uint32 *data2 = new uint32[5];
+        for (int i = 0; i < 5; i++) {
+           data2[i] = i * 5;
+        }
         
-        // Generator g3("{\"dtype\":\"uint32\",\"length\": 5}");
-        // Schema schema3;
-        // g3.walk(schema3);
-        // Node n3(schema3,data2,true); // true for external
+        Generator g3("dtype: uint32\nlength: 5", "conduit_yaml");
+        Schema schema3;
+        g3.walk(schema3);
+        Node n3(schema3,data2,true); // true for external
         
-        // for (int i = 0; i < 5; i++) {
-        //    EXPECT_EQ(n3.as_uint32_ptr()[i], i*5);
-        // }
+        for (int i = 0; i < 5; i++) {
+           EXPECT_EQ(n3.as_uint32_ptr()[i], i*5);
+        }
         
-        // Generator g4("[\"uint32\", \"float64\", \"uint32\"]");
+        Generator g4("- uint32\n- float64\n- uint32", "conduit_yaml");
         
-        // char* data3 = new char[16];
-        // memcpy(&data3[0],&a_val,4);
-        // memcpy(&data3[4],&c_val,8);
-        // memcpy(&data3[12],&b_val,4);
-        // Schema schema4;
-        // g4.walk(schema4);
-        // Node n4(schema4,data3,true); // true for external
-        // EXPECT_EQ(n4[0].as_uint32(),a_val);
-        // EXPECT_EQ(n4[1].as_float64(),c_val);
-        // EXPECT_EQ(n4[2].as_uint32(),b_val);
+        char* data3 = new char[16];
+        memcpy(&data3[0],&a_val,4);
+        memcpy(&data3[4],&c_val,8);
+        memcpy(&data3[12],&b_val,4);
+        Schema schema4;
+        g4.walk(schema4);
+        Node n4(schema4,data3,true); // true for external
+        EXPECT_EQ(n4[0].as_uint32(),a_val);
+        EXPECT_EQ(n4[1].as_float64(),c_val);
+        EXPECT_EQ(n4[2].as_uint32(),b_val);
         
-        // Generator g5("{\"top\":[{\"int1\":\"uint32\", \"int2\":\"uint32\"}, \"float64\", \"uint32\"], \"other\":\"float64\"}");
-        // Schema schema5;
-        // g5.walk(schema5);
+        Generator g5("top:\n- int1: uint32\n  int2: uint32\n- float64\n- uint32\nother: float64", "conduit_yaml");
+        Schema schema5;
+        g5.walk(schema5);
             
-        // char* data4 = new char[28];
-        // uint32   d_val  = 40;
-        // float64  e_val  = 50.0;
-        // memcpy(&data4[0],&a_val,4);
-        // memcpy(&data4[4],&b_val,4);
-        // memcpy(&data4[8],&c_val,8);
-        // memcpy(&data4[16],&d_val,4);
-        // memcpy(&data4[20],&e_val,8);
+        char* data4 = new char[28];
+        uint32   d_val  = 40;
+        float64  e_val  = 50.0;
+        memcpy(&data4[0],&a_val,4);
+        memcpy(&data4[4],&b_val,4);
+        memcpy(&data4[8],&c_val,8);
+        memcpy(&data4[16],&d_val,4);
+        memcpy(&data4[20],&e_val,8);
         
-        // Node n5(schema5,data4,true); // true for external
+        Node n5(schema5,data4,true); // true for external
         
-        // n5.schema().print();
-        // EXPECT_EQ(n5["top"][0]["int1"].as_uint32(),a_val);
-        // EXPECT_EQ(n5["top"][0]["int2"].as_uint32(),b_val);
-        // EXPECT_EQ(n5["top"][1].as_float64(),c_val);
-        // EXPECT_EQ(n5["top"][2].as_uint32(),d_val);
-        // EXPECT_EQ(n5["other"].as_float64(),e_val);
+        n5.schema().print();
+        EXPECT_EQ(n5["top"][0]["int1"].as_uint32(),a_val);
+        EXPECT_EQ(n5["top"][0]["int2"].as_uint32(),b_val);
+        EXPECT_EQ(n5["top"][1].as_float64(),c_val);
+        EXPECT_EQ(n5["top"][2].as_uint32(),d_val);
+        EXPECT_EQ(n5["other"].as_float64(),e_val);
         
         
-        // delete [] data;
-        // delete [] data2;
-        // delete [] data3;
-        // delete [] data4;
+        delete [] data;
+        delete [] data2;
+        delete [] data3;
+        delete [] data4;
     }
 
 }
 
-// //-----------------------------------------------------------------------------
-// TEST(conduit_generator, simple_gen_schema_with_gen_setters)
-// {
-//     uint32   a_val = 10;
-//     uint32   b_val = 20;
-//     float64  c_val = 30.0;
-
-//     char *data = new char[16];
-//     memcpy(&data[0], &a_val, 4);
-//     memcpy(&data[4], &b_val, 4);
-//     memcpy(&data[8], &c_val, 8);
-
-//     Generator g;
-
-//     std::string s1_str = "{\"a\":\"uint32\",\"b\":\"uint32\",\"c\":\"float64\"}";
-
-//     g.set_schema(s1_str);
-//     g.set_data_ptr(data);
-
-//     EXPECT_EQ(g.schema(), s1_str);
-//     EXPECT_EQ(g.protocol(), std::string("conduit_json"));
-//     EXPECT_EQ(g.data_ptr(), data);
+//-----------------------------------------------------------------------------
+TEST(conduit_generator, simple_gen_schema_with_gen_setters)
+{
+    // JSON
+    {
+        uint32   a_val = 10;
+        uint32   b_val = 20;
+        float64  c_val = 30.0;
     
-//     Node n;
-//     g.walk(n);
-
-//     EXPECT_EQ(n["a"].as_uint32(), a_val);
-//     EXPECT_EQ(n["b"].as_uint32(), b_val);
-//     EXPECT_EQ(n["c"].as_float64(), c_val);
-
-//     std::string s2_str = "{\"g\": {\"a\":\"uint32\",\"b\":\"uint32\",\"c\":\"float64\"}}";
-//     std::cout << s2_str << std::endl;
-
-//     g.set_schema(s2_str);
-//     g.set_data_ptr(NULL);
-
-//     Schema schema2;
-//     g.walk(schema2);
-
-//     Node n2(schema2, data, true); // true for external
-//     EXPECT_EQ(n2["g"]["a"].as_uint32(), a_val);
-//     EXPECT_EQ(n2["g"]["b"].as_uint32(), b_val);
-//     EXPECT_EQ(n2["g"]["c"].as_float64(), c_val);
-
-//     uint32 *data2 = new uint32[5];
-//     for (int i = 0; i < 5; i++) {
-//         data2[i] = i * 5;
-//     }
-
-//     g.set_schema("{\"dtype\":\"uint32\",\"length\":  5}");
-
-//     Schema schema3;
-//     g.walk(schema3);
-//     Node n3(schema3, data2, true); // true for external
-
-//     for (int i = 0; i < 5; i++) {
-//         EXPECT_EQ(n3.as_uint32_ptr()[i], i * 5);
-//     }
-
-//     g.set_schema("[\"uint32\", \"float64\", \"uint32\"]");
-
-//     char* data3 = new char[16];
-//     memcpy(&data3[0], &a_val, 4);
-//     memcpy(&data3[4], &c_val, 8);
-//     memcpy(&data3[12], &b_val, 4);
-//     Schema schema4;
-//     g.walk(schema4);
-//     Node n4(schema4, data3, true); // true for external
-//     EXPECT_EQ(n4[0].as_uint32(), a_val);
-//     EXPECT_EQ(n4[1].as_float64(), c_val);
-//     EXPECT_EQ(n4[2].as_uint32(), b_val);
-
-//     g.set_schema("{\"top\":[{\"int1\":\"uint32\", \"int2\":\"uint32\"}, \"float64\", \"uint32\"], \"other\":\"float64\"}");
+        char *data = new char[16];
+        memcpy(&data[0], &a_val, 4);
+        memcpy(&data[4], &b_val, 4);
+        memcpy(&data[8], &c_val, 8);
     
-//     Schema schema5;
-//     g.walk(schema5);
+        Generator g;
+    
+        std::string s1_str = "{\"a\":\"uint32\",\"b\":\"uint32\",\"c\":\"float64\"}";
 
-//     char* data4 = new char[28];
-//     uint32   d_val = 40;
-//     float64  e_val = 50.0;
-//     memcpy(&data4[0], &a_val, 4);
-//     memcpy(&data4[4], &b_val, 4);
-//     memcpy(&data4[8], &c_val, 8);
-//     memcpy(&data4[16], &d_val, 4);
-//     memcpy(&data4[20], &e_val, 8);
+        g.set_schema(s1_str);
+        g.set_data_ptr(data);
+    
+        EXPECT_EQ(g.schema(), s1_str);
+        EXPECT_EQ(g.protocol(), std::string("conduit_json"));
+        EXPECT_EQ(g.data_ptr(), data);
+        
+        Node n;
+        g.walk(n);
+    
+        EXPECT_EQ(n["a"].as_uint32(), a_val);
+        EXPECT_EQ(n["b"].as_uint32(), b_val);
+        EXPECT_EQ(n["c"].as_float64(), c_val);
+    
+        std::string s2_str = "{\"g\": {\"a\":\"uint32\",\"b\":\"uint32\",\"c\":\"float64\"}}";
+        std::cout << s2_str << std::endl;
+    
+        g.set_schema(s2_str);
+        g.set_data_ptr(NULL);
+    
+        Schema schema2;
+        g.walk(schema2);
+    
+        Node n2(schema2, data, true); // true for external
+        EXPECT_EQ(n2["g"]["a"].as_uint32(), a_val);
+        EXPECT_EQ(n2["g"]["b"].as_uint32(), b_val);
+        EXPECT_EQ(n2["g"]["c"].as_float64(), c_val);
+    
+        uint32 *data2 = new uint32[5];
+        for (int i = 0; i < 5; i++) {
+            data2[i] = i * 5;
+        }
+    
+        g.set_schema("{\"dtype\":\"uint32\",\"length\":  5}");
+    
+        Schema schema3;
+        g.walk(schema3);
+        Node n3(schema3, data2, true); // true for external
+    
+        for (int i = 0; i < 5; i++) {
+            EXPECT_EQ(n3.as_uint32_ptr()[i], i * 5);
+        }
+    
+        g.set_schema("[\"uint32\", \"float64\", \"uint32\"]");
+    
+        char* data3 = new char[16];
+        memcpy(&data3[0], &a_val, 4);
+        memcpy(&data3[4], &c_val, 8);
+        memcpy(&data3[12], &b_val, 4);
+        Schema schema4;
+        g.walk(schema4);
+        Node n4(schema4, data3, true); // true for external
+        EXPECT_EQ(n4[0].as_uint32(), a_val);
+        EXPECT_EQ(n4[1].as_float64(), c_val);
+        EXPECT_EQ(n4[2].as_uint32(), b_val);
+    
+        g.set_schema("{\"top\":[{\"int1\":\"uint32\", \"int2\":\"uint32\"}, \"float64\", \"uint32\"], \"other\":\"float64\"}");
+        
+        Schema schema5;
+        g.walk(schema5);
+    
+        char* data4 = new char[28];
+        uint32   d_val = 40;
+        float64  e_val = 50.0;
+        memcpy(&data4[0], &a_val, 4);
+        memcpy(&data4[4], &b_val, 4);
+        memcpy(&data4[8], &c_val, 8);
+        memcpy(&data4[16], &d_val, 4);
+        memcpy(&data4[20], &e_val, 8);
+    
+        Node n5(schema5, data4, true); // true for external
+    
+        n5.schema().print();
+        EXPECT_EQ(n5["top"][0]["int1"].as_uint32(), a_val);
+        EXPECT_EQ(n5["top"][0]["int2"].as_uint32(), b_val);
+        EXPECT_EQ(n5["top"][1].as_float64(), c_val);
+        EXPECT_EQ(n5["top"][2].as_uint32(), d_val);
+        EXPECT_EQ(n5["other"].as_float64(), e_val);
+    
+    
+        delete[] data;
+        delete[] data2;
+        delete[] data3;
+        delete[] data4;
+    }
 
-//     Node n5(schema5, data4, true); // true for external
+    // YAML
+    {
+        uint32   a_val = 10;
+        uint32   b_val = 20;
+        float64  c_val = 30.0;
+    
+        char *data = new char[16];
+        memcpy(&data[0], &a_val, 4);
+        memcpy(&data[4], &b_val, 4);
+        memcpy(&data[8], &c_val, 8);
+    
+        Generator g;
+    
+        std::string s1_str = "a: uint32\nb: uint32\nc: float64";
+    
+        g.set_schema(s1_str);
+        g.set_protocol("conduit_yaml");
+        g.set_data_ptr(data);
+    
+        EXPECT_EQ(g.schema(), s1_str);
+        EXPECT_EQ(g.protocol(), std::string("conduit_yaml"));
+        EXPECT_EQ(g.data_ptr(), data);
+        
+        Node n;
+        g.walk(n);
+    
+        EXPECT_EQ(n["a"].as_uint32(), a_val);
+        EXPECT_EQ(n["b"].as_uint32(), b_val);
+        EXPECT_EQ(n["c"].as_float64(), c_val);
+    
+        std::string s2_str = "g: \n  a: uint32\n  b: uint32\n  c: float64";
+        std::cout << s2_str << std::endl;
+    
+        g.set_schema(s2_str);
+        g.set_data_ptr(NULL);
+        g.set_protocol("conduit_yaml");
+    
+        Schema schema2;
+        g.walk(schema2);
+    
+        Node n2(schema2, data, true); // true for external
+        EXPECT_EQ(n2["g"]["a"].as_uint32(), a_val);
+        EXPECT_EQ(n2["g"]["b"].as_uint32(), b_val);
+        EXPECT_EQ(n2["g"]["c"].as_float64(), c_val);
+    
+        uint32 *data2 = new uint32[5];
+        for (int i = 0; i < 5; i++) {
+            data2[i] = i * 5;
+        }
+    
+        g.set_schema("dtype: uint32\nlength: 5");
+        g.set_protocol("conduit_yaml");
+    
+        Schema schema3;
+        g.walk(schema3);
+        Node n3(schema3, data2, true); // true for external
+    
+        for (int i = 0; i < 5; i++) {
+            EXPECT_EQ(n3.as_uint32_ptr()[i], i * 5);
+        }
+    
+        g.set_schema("- uint32\n- float64\n- uint32");
+        g.set_protocol("conduit_yaml");
 
-//     n5.schema().print();
-//     EXPECT_EQ(n5["top"][0]["int1"].as_uint32(), a_val);
-//     EXPECT_EQ(n5["top"][0]["int2"].as_uint32(), b_val);
-//     EXPECT_EQ(n5["top"][1].as_float64(), c_val);
-//     EXPECT_EQ(n5["top"][2].as_uint32(), d_val);
-//     EXPECT_EQ(n5["other"].as_float64(), e_val);
+        char* data3 = new char[16];
+        memcpy(&data3[0], &a_val, 4);
+        memcpy(&data3[4], &c_val, 8);
+        memcpy(&data3[12], &b_val, 4);
+        Schema schema4;
+        g.walk(schema4);
+        Node n4(schema4, data3, true); // true for external
+        EXPECT_EQ(n4[0].as_uint32(), a_val);
+        EXPECT_EQ(n4[1].as_float64(), c_val);
+        EXPECT_EQ(n4[2].as_uint32(), b_val);
 
+        g.set_schema("top:\n- int1: uint32\n  int2: uint32\n- float64\n- uint32\nother: float64");
+        g.set_protocol("conduit_yaml");
 
-//     delete[] data;
-//     delete[] data2;
-//     delete[] data3;
-//     delete[] data4;
-
-// }
+        Schema schema5;
+        g.walk(schema5);
+    
+        char* data4 = new char[28];
+        uint32   d_val = 40;
+        float64  e_val = 50.0;
+        memcpy(&data4[0], &a_val, 4);
+        memcpy(&data4[4], &b_val, 4);
+        memcpy(&data4[8], &c_val, 8);
+        memcpy(&data4[16], &d_val, 4);
+        memcpy(&data4[20], &e_val, 8);
+    
+        Node n5(schema5, data4, true); // true for external
+    
+        n5.schema().print();
+        EXPECT_EQ(n5["top"][0]["int1"].as_uint32(), a_val);
+        EXPECT_EQ(n5["top"][0]["int2"].as_uint32(), b_val);
+        EXPECT_EQ(n5["top"][1].as_float64(), c_val);
+        EXPECT_EQ(n5["top"][2].as_uint32(), d_val);
+        EXPECT_EQ(n5["other"].as_float64(), e_val);
+    
+    
+        delete[] data;
+        delete[] data2;
+        delete[] data3;
+        delete[] data4;
+    }
+}
 
 
 // //-----------------------------------------------------------------------------
